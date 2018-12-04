84fde546bd3ebf85694d729a210fb01bc9c96afe
Kamil Tomšík <info@tomsik.cz>
2018-12-04 22:31:31 +0100

    update example

--- example/example.tsx
+++ example/example.tsx
@@ -3,13 +3,35 @@ import { Window } from '..'
 import { View, Button, Text, render } from '../src/react'
 
 const App = () =>
-  <View style={{ flexDirection: 'row' }}>
-    <View style={{ flex: 1, padding: 10, backgroundColor: '#dddddd' }}>
-      <Text>SIDEBAR</Text>
+  <View style={styles.container}>
+    <View style={styles.sidebar}>
+      <Text>Sidebar</Text>
+
+      <Text>Lorem ipsum dolor sit amet</Text>
     </View>
-    <View style={{ flex: 2, padding: 10 }}>
-      <Button title="HELLO" />
+    <View style={styles.main}>
+      <Text>Main content</Text>
+
+      <Button title="Click me" />
     </View>
   </View>
+
+const styles = {
+  container: {
+    flexDirection: 'row'
+  },
+
+  sidebar: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#eeeeee',
+    justifyContent: 'space-between'
+  },
+
+  main: {
+    flex: 2,
+    padding: 20
+  }
+}
 
 render(<App />, new Window("Example"))
