216d2b631a3ff41d2d902b1b1c4ddd502ed335f1
Kamil Tomšík <info@tomsik.cz>
2018-12-08 00:10:04 +0100

    add react-calculator example

---
+++ example/react-calculator.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { useState } from 'react'
+import { Window } from '..'
+import { render, View, Text, Button } from '../src/react'
+
+const App = () => {
+  const [expr, setExpr] = useState('0')
+
+  const CaButton = ({ ch }) =>
+    <View style={styles.caButton}>
+      <Button title={ch} onPress={() => setExpr(expr + ch)} />
+    </View>
+
+  return (
+    <View>
+      <Display value={expr} />
+
+      <View style={styles.buttons}>
+        <CaButton ch="7" />
+        <CaButton ch="8" />
+        <CaButton ch="9" />
+        <CaButton ch="*" />
+
+        <CaButton ch="4" />
+        <CaButton ch="5" />
+        <CaButton ch="6" />
+        <CaButton ch="-" />
+
+        <CaButton ch="1" />
+        <CaButton ch="2" />
+        <CaButton ch="3" />
+        <CaButton ch="+" />
+
+        <CaButton ch="0" />
+        <CaButton ch="," />
+        <CaButton ch="/" />
+        <CaButton ch="=" />
+      </View>
+    </View>
+  )
+}
+
+const Display = ({ value }) =>
+  <View style={styles.display}>
+    <Text style={styles.displayText}>{value}</Text>
+  </View>
+
+const styles = {
+  display: {
+    height: 100,
+    padding: 10,
+    justifyContent: 'flex-end',
+    backgroundColor: '#000000'
+  },
+
+  displayText: {
+    color: '#ffffff'
+  },
+
+  buttons: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+
+  caButton: {
+    width: '25%'
+  }
+}
+
+render(<App />, new Window("Calculator", 400, 300))
