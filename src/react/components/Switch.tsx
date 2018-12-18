10c5e5922f58f8a7e5f9c2ddf69a1d59b86dc628
Kamil Tomšík <info@tomsik.cz>
2018-12-18 23:10:20 +0100

    update Switch

--- src/react/components/Switch.tsx
+++ src/react/components/Switch.tsx
@@ -2,28 +2,43 @@ import * as React from 'react'
 import { SwitchProps } from '../react-native-types'
 import { View, Text, StyleSheet } from '..'
 
-const Switch = (props: SwitchProps) =>
-  <View style={[styles.track, props.disabled && styles.trackDisabled]}>
-    <View style={[styles.thumb, props.value && styles.thumbOn]}></View>
-  </View>
+const Switch = (props: SwitchProps) => {
+  const { disabled, value } = props
+
+  return (
+    <View style={[styles.track, value && styles.trackActive, disabled && styles.trackDisabled]}>
+      <View style={[styles.thumb, value && styles.thumbActive]} />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
+  // TODO: disabling might be done using opacity and/or color filter
+
   track: {
-    width: 100,
-    backgroundColor: '#a3d3cf'
+    width: 80,
+    height: 28,
+    padding: 3,
+
+    backgroundColor: '#bdbdbd'
+  },
+
+  trackActive: {
+    backgroundColor: '#2196f3'
   },
 
   trackDisabled: {
-    backgroundColor: '#d5d5d5'
+    backgroundColor: '#f5f5f5'
   },
 
   thumb: {
-    width: 40,
+    height: '100%',
+    width: 35,
     backgroundColor: '#ffffff'
   },
 
-  thumbOn: {
-    marginLeft: 60
+  thumbActive: {
+    marginLeft: 39
   }
 })
 
