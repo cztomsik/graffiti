17dd0410862973e5782bb207284af28a6c29c955
Kamil Tomšík <info@tomsik.cz>
2019-01-01 23:24:13 +0100

    add TextInput

---
+++ src/react/components/TextInput.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { useState, useRef, useContext } from 'react'
+import { TextInputProps } from '../react-native-types'
+import { ControlManagerContext, TouchableWithoutFeedback, Text, StyleSheet } from '..'
+import { ResourceManager } from '../..'
+
+// TODO: reconsider if layout should be coupled to Surface, drawing custom shapes (caret) would have been much easier
+
+const TextInput = (props: TextInputProps) => {
+  const textInput = useTextInput(props.value, props.onChangeText)
+
+  return (
+    <TouchableWithoutFeedback style={styles.input} onPress={textInput.focus}>
+      <Text>{props.value}</Text>
+    </TouchableWithoutFeedback>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: '#cccccc',
+    borderBottomWidth: 1
+  }
+})
+
+export default TextInput
+
+
+const useTextInput = (value, onChange) => {
+  const controlManager = useContext(ControlManagerContext)
+  const textInputRef = useRef(undefined)
+
+  if (textInputRef.current === undefined) {
+    textInputRef.current = createTextInput(controlManager)
+  }
+
+  const textInput = textInputRef.current
+
+  textInput.value = value
+  textInput.onChange = onChange
+
+  return textInput
+}
+
+const createTextInput = (controlManager) => {
+  const handler = {
+    keyPress: (e) => {
+      // backspace
+      if (e.ch === '\u007f') {
+        return textInput.onChange(textInput.value = textInput.value.slice(0, -1))
+      }
+
+      if ( ! e.ch) {
+        return
+      }
+
+      textInput.onChange(textInput.value = textInput.value + e.ch)
+    },
+    focus: () => {},
+    blur: () => {}
+  }
+
+  const textInput = {
+    value: '',
+    onChange: (str) => {},
+    focus: () => controlManager.focus(handler),
+    // TODO: only if we're focused
+    blur: () => controlManager.blur()
+  }
+
+  return textInput
+}
