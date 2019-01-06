331c0c491ddb1b385134a2a89578c537a50736b5
Kamil Tomšík <info@tomsik.cz>
2019-01-06 20:00:43 +0100

    add brush/clip/layout style caching

--- src/react/Stylesheet.ts
+++ src/react/Stylesheet.ts
@@ -1,13 +1,16 @@
 import { RNStyleSheet, StyleProp, ViewStyle, TextStyle } from './react-native-types'
+import { ResourceManager } from '../core'
 
 // needed for Stylesheet.create() autocompletion
 type Styles = {
-  [key: string]: StyleProp<ViewStyle | TextStyle>;
+  [key: string]: FlatStyle;
 }
+
+type FlatStyle = ViewStyle | TextStyle
 
 const create = (obj: Styles): Styles => {
   for (const k in obj) {
-    Object.freeze(obj[k])
+    compile(obj[k], ++lastId)
   }
 
   return obj as any
@@ -19,10 +22,35 @@ const flatten: typeof RNStyleSheet.flatten = styles => {
   return Array.isArray(styles) ? Object.assign({}, ...styles) : styles
 }
 
+const cachingFlatten: typeof flatten = (styles) => {
+  const id = [].concat(styles).filter(Boolean).map(s => s._id || JSON.stringify(s)).join('-')
+
+  let res = CACHE.get(id)
+
+  if (res === undefined) {
+    res = flatten(styles)
+    compile(res, id)
+  }
+
+  return res
+}
+
+const compile = (style, id) => {
+  style._brush = ResourceManager.getBrush(style)
+  style._layout = ResourceManager.getLayout(style)
+  style._clip = ResourceManager.getClip(style)
+
+  CACHE.set(style._id = id, Object.freeze(style))
+}
+
+// TODO: LeakingCache
+const CACHE = new Map()
+let lastId = 0
+
 const StyleSheet = {
   compose: (left, right) => (left && right ? [left, right] : left || right),
   // note that react-native does not return numbers anymore,
-  flatten,
+  flatten: cachingFlatten,
   create
 }
 
