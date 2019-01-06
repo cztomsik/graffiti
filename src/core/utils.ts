1ede27d0bd4cfc0ed65dc5065a4d0eb72cf1682b
Kamil Tomšík <info@tomsik.cz>
2019-01-06 22:41:42 +0100

    guard text changes

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -5,12 +5,20 @@ export const remove = (arr: any[], item: any) =>
 
 // TODO: rgb(), rgba(), hex short
 export const parseColor = (str: string): BridgeColor => {
-  return [
-    parseHex(str.slice(1, 3)) / 255,
-    parseHex(str.slice(3, 5)) / 255,
-    parseHex(str.slice(5, 7)) / 255,
-    1
-  ]
+  let res = COLOR_CACHE.get(str)
+
+  if (res === undefined) {
+    COLOR_CACHE.set(str, res = [
+      parseHex(str.slice(1, 3)) / 255,
+      parseHex(str.slice(3, 5)) / 255,
+      parseHex(str.slice(5, 7)) / 255,
+      1
+    ])
+  }
+
+  return res
 }
 
 export const parseHex = (str: string) => parseInt(str, 16)
+
+const COLOR_CACHE = new Map<string, BridgeColor>()
