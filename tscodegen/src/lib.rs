325a9af330da8348b080004340d38a065d3daaa1
Kamil Tomšík <info@tomsik.cz>
2019-02-18 22:42:01 +0100

    poc of tscodegen in rust

--- tscodegen/src/lib.rs
+++ tscodegen/src/lib.rs
@@ -1,23 +1,20 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
 use std::fs::OpenOptions;
-use syn::{parse, DeriveInput, Path, Type, TypePath};
-//use std::io::prelude::*;
-use std::fmt;
-use serde::Serialize;
-use serde_json;
 use std::io::prelude::*;
+use syn::{parse, DeriveInput, Ident, Type};
 
 #[proc_macro_attribute]
 pub fn tscodegen(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input: DeriveInput = parse(item.clone())
         .expect("couldn't parse, attribute is probably not placed on enum/struct");
 
-    let name = input.ident.to_string();
-
+    // 1. prepare intermediate representation
+    let name = input.ident.clone();
     let ts_type = match input.data {
         syn::Data::Enum(syn::DataEnum { variants, .. }) => {
-            let members = variants.iter().map(|v| v.ident.to_string()).collect();
+            let members = variants.iter().map(|v| v.ident.clone()).collect();
 
             TsType::Enum { name, members }
         }
@@ -28,20 +25,15 @@ pub fn tscodegen(_attr: TokenStream, item: TokenStream) -> TokenStream {
             match is_tuple {
                 true => TsType::TupleStruct {
                     name,
-                    types: fields
-                        .iter()
-                        .map(|f| to_ts_type(&f.ty))
-                        .collect(),
+                    types: fields.iter().map(|f| f.ty.clone()).collect(),
                 },
 
                 false => TsType::Struct {
                     name: name.clone(),
-                    properties: fields.iter().map(|f| {
-                        let key = f.ident.clone().unwrap().to_string();
-                        let ts_type = to_ts_type(&f.ty);
-
-                        (key, ts_type)
-                    }).collect(),
+                    properties: fields
+                        .iter()
+                        .map(|f| (f.ident.clone().unwrap(), f.ty.clone()))
+                        .collect(),
                 },
             }
         }
@@ -49,55 +41,95 @@ pub fn tscodegen(_attr: TokenStream, item: TokenStream) -> TokenStream {
         _ => panic!("unexpected data {:?}", input.data),
     };
 
+    // 2. generate "code" using quasiquotes
+    let ts = match ts_type {
+        TsType::Enum { name, members } => {
+            let write_name = Ident::new(format!("write{}", name).as_ref(), name.span());
+
+            quote! {
+                export enum #name {
+                    #(#members),*
+                }
+
+                export function #write_name(buf: Buffer, value: #name) {
+                    buf.writeInt8(0, value)
+                }
+            }
+        }
+
+        TsType::TupleStruct { name, types } => {
+            let len = types.len();
+            let mk_name = Ident::new(format!("mk{}", name).as_ref(), name.span());
+            let write_name = Ident::new(format!("write{}", name).as_ref(), name.span());
+
+            quote! {
+                export interface #name {
+                    length: #len
+                }
+
+                export function #mk_name(todoParams) {
+                    return []
+                }
+
+                export function #write_name() {
+                    // TODO
+                }
+            }
+        }
+
+        TsType::Struct { name, properties } => {
+            let properties = properties.iter().map(|(name, ty)| {
+                quote! {
+                    #name: #ty;
+                }
+            });
+            let mk_name = Ident::new(format!("mk{}", name).as_ref(), name.span());
+            let write_name = Ident::new(format!("write{}", name).as_ref(), name.span());
+
+            quote! {
+                export interface #name {
+                    #(#properties)*
+                }
+
+                export function #mk_name() {
+                    return {
+                        // TODO
+                    }
+                }
+
+                export function #write_name() {
+                    // TODO
+                }
+            }
+        }
+    };
+
     let mut file = OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
-        .open(format!("generated/{}.json", &input.ident))
+        .open(format!("generated/{}.ts", &input.ident))
         .expect("cannot open/create codegen file");
-    
-    file.write_all(&serde_json::to_vec_pretty(&ts_type).unwrap());
+
+    write!(file, "{}", ts).expect("couldnt write");
 
     item
 }
 
-fn to_ts_type(ty: &syn::Type) -> String {
-    match ty {
-        Type::Path(TypePath {
-                       path: Path { segments, .. },
-                       ..
-                   }) => {
-            let parts: Vec<String> =
-                segments.iter().map(|s| s.ident.to_string()).collect();
-            parts.join(".")
-        }
-
-        _ => panic!("unsupported type {:#?}", ty),
-    }
-}
-
-#[derive(Serialize, Debug)]
+#[derive(Debug)]
 enum TsType {
     Enum {
-        name: String,
-        members: Vec<String>,
+        name: Ident,
+        members: Vec<Ident>,
     },
 
     Struct {
-        name: String,
-        properties: Vec<(String, String)>,
+        name: Ident,
+        properties: Vec<(Ident, Type)>,
     },
 
     TupleStruct {
-        name: String,
-        types: Vec<String>,
+        name: Ident,
+        types: Vec<Type>,
     }, // TODO: discriminated union (hash with "type")
-}
-
-struct TsIdent(String);
-
-impl fmt::Debug for TsIdent {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str(&self.0)
-    }
 }
