db45a5f8f15427b2353295bfe4a89fc0f70eab33
Kamil Tomšík <info@tomsik.cz>
2019-02-28 23:12:08 +0100

    add render service

--- native-new/src/lib.rs
+++ native-new/src/lib.rs
@@ -1,29 +1,35 @@
 mod generated;
 mod surface;
+mod render;
+mod layout;
+mod temp;
 
 // TODO: generics
 // (into() is not enough; maybe struct)
 // usize for now
 pub type Id = usize;
 
-mod layout;
-mod temp;
-
-/*
-use crate::layout::{update_layout, LayoutMsg};
+use crate::generated::{Msg};
 use serde::Deserialize;
 use serde_json;
-use yoga::{FlexStyle, Node as YogaNode, StyleUnit};
+use crate::layout::LayoutService;
+use crate::surface::SurfaceService;
+use crate::render::RenderService;
+use crate::generated::SurfaceId;
+use crate::layout::YogaLayoutService;
+use crate::render::WebrenderRenderService;
 
-static mut APP: Option<App> = None;
+static mut APP: Option<Box<App>> = None;
 
 #[no_mangle]
 pub extern "C" fn init() {
     temp::init();
 
-    let layout: Vec<YogaNode> = Vec::new();
-
-    unsafe { APP = Some(App { layout }) }
+    unsafe { APP = Some(Box::new(App {
+        surface_service: SurfaceService::new(),
+        layout_service: YogaLayoutService::new(),
+        render_service: WebrenderRenderService::new()
+    })) }
 }
 
 #[no_mangle]
@@ -37,22 +43,24 @@ pub extern "C" fn send(data: *const u8, len: u32) {
             None => {}
             Some(ref mut app) => match msg {
                 Msg::HandleEvents => temp::handle_events(),
-                Msg::Alloc => app.layout.push(YogaNode::new()),
-                Msg::UpdateLayout(msgs) => update_layout(&mut app.layout, msgs),
+                Msg::AppendChild { parent, child } => {
+                    app.surface_service.append_child(parent.into(), child.into())
+                }
+                Msg::SetBackgroundColor { surface, color } => app.surface_service.set_background_color(surface.into(), color),
+                _ => {}
             },
         }
     }
 }
 
-#[derive(Deserialize)]
-#[serde(tag = "tag", content = "value")]
-enum Msg {
-    HandleEvents,
-    Alloc,
-    UpdateLayout(Vec<LayoutMsg>),
+struct App {
+    surface_service: SurfaceService,
+    layout_service: YogaLayoutService,
+    render_service: WebrenderRenderService
 }
 
-struct App {
-    layout: Vec<YogaNode>,
+impl Into<Id> for SurfaceId {
+    fn into(self) -> Id {
+        self.0
+    }
 }
-*/
---
+++ native-new/src/render/webrender.rs
@@ -0,0 +1,195 @@
+use super::{Border, BoxShadow, Color, ComputedLayout, Data, Image, RenderService, Text};
+use crate::Id;
+use webrender::api::{
+    AlphaType, BorderDetails, BorderDisplayItem, BorderRadius, BorderSide, BorderStyle,
+    BoxShadowClipMode, BoxShadowDisplayItem, ColorF, DisplayListBuilder, FontInstanceKey,
+    ImageDisplayItem, ImageKey, ImageRendering, LayoutPrimitiveInfo, LayoutRect, LayoutSize,
+    LayoutVector2D, NormalBorder, PipelineId, RectangleDisplayItem, SpaceAndClipInfo,
+    SpecificDisplayItem, TextDisplayItem, ColorU, IdNamespace
+};
+use webrender::euclid::{TypedPoint2D, TypedSideOffsets2D, TypedSize2D};
+
+pub struct WebrenderRenderService {}
+
+impl WebrenderRenderService {
+    pub fn new() -> Self {
+        WebrenderRenderService {}
+    }
+}
+
+impl RenderService for WebrenderRenderService {
+    fn render(&mut self, id: Id, data: &Data) {
+        let content_size = LayoutSize::new(1024., 768.);
+        let pipeline_id = PipelineId::dummy();
+        let mut builder = DisplayListBuilder::new(pipeline_id, content_size);
+
+        // surface specific
+        let layout = LayoutPrimitiveInfo::new(TypedSize2D::new(100., 100.).into());
+        // TODO: clip (normal, border-radius, scrollframe)
+        let space_and_clip = SpaceAndClipInfo::root_scroll(pipeline_id);
+
+        // shared, not directly rendered
+        if let Some(border_radius) = data.border_radii.get(&id) {
+            // TODO: set to context
+        }
+
+        // TODO: hittest
+
+        render_item(data.box_shadows.get(&id), &mut builder, &layout, &space_and_clip);
+
+        if let Some(color) = data.background_colors.get(&id) {
+            render_item(Some(&BackgroundColor(color.clone())), &mut builder, &layout, &space_and_clip);
+        }
+
+        render_item(data.images.get(&id), &mut builder, &layout, &space_and_clip);
+
+        // selection should be below text
+        // TODO
+        // render_item(data.selections.get(&id), &mut builder, &layout, &space_and_clip);
+
+        render_item(data.texts.get(&id), &mut builder, &layout, &space_and_clip);
+
+        if let Some(children) = data.children.get(id) {
+            for child_id in children {
+                // TODO: layout, space_and_clip, border_radius
+                //render(
+                //    child_id,
+                //    box_shadows,
+                //    background_colors,
+                //    ...
+                //)
+            }
+        }
+    }
+}
+
+fn render_item<T>(item: Option<&T>, builder: &mut DisplayListBuilder, layout: &LayoutPrimitiveInfo, space_and_clip: &SpaceAndClipInfo) where T: RenderItem {
+    if let Some(item) = item {
+        item.render_using(builder, layout, space_and_clip);
+    }
+}
+
+trait RenderItem {
+    // so that we can test it
+    fn render(&self) -> SpecificDisplayItem;
+
+    // so that we can customize it if necessary (push_iter() for text)
+    // NOTE: trait can be resolved statically (in contrast to match clause)
+    fn render_using(
+        &self,
+        builder: &mut DisplayListBuilder,
+        layout: &LayoutPrimitiveInfo,
+        space_and_clip: &SpaceAndClipInfo,
+    ) {
+        builder.push_item(&self.render(), layout, space_and_clip);
+    }
+}
+
+impl RenderItem for BoxShadow {
+    fn render(&self) -> SpecificDisplayItem {
+        SpecificDisplayItem::BoxShadow(BoxShadowDisplayItem {
+            color: self.color.clone().into(),
+
+            // TODO
+            box_bounds: LayoutRect::new(TypedPoint2D::new(0., 0.), TypedSize2D::new(100., 100.)),
+            offset: [self.offset.0, self.offset.1].into(),
+            blur_radius: self.blur,
+            spread_radius: self.spread,
+
+            // TODO
+            border_radius: BorderRadius::uniform(5.0),
+
+            // TODO
+            clip_mode: BoxShadowClipMode::Outset,
+        })
+    }
+}
+
+// just so that we can implement the trait
+struct BackgroundColor(Color);
+
+impl RenderItem for BackgroundColor {
+    fn render(&self) -> SpecificDisplayItem {
+        SpecificDisplayItem::Rectangle(RectangleDisplayItem {
+            color: self.0.clone().into(),
+        })
+    }
+}
+
+// TODO
+impl RenderItem for Text {
+    fn render(&self) -> SpecificDisplayItem {
+        // TODO
+        let font_key = FontInstanceKey::new(IdNamespace(0), 0);
+
+        SpecificDisplayItem::Text(TextDisplayItem {
+            font_key,
+            color: self.color.clone().into(),
+            glyph_options: None,
+        })
+    }
+
+    // TODO: push_iter(&glyphs)
+}
+
+impl RenderItem for Image {
+    fn render(&self) -> SpecificDisplayItem {
+        // TODO
+        let image_key = ImageKey::DUMMY;
+        let layout = LayoutPrimitiveInfo::new(TypedSize2D::new(0., 0.).into());
+
+        SpecificDisplayItem::Image(ImageDisplayItem {
+            image_key,
+            stretch_size: layout.clone().rect.size,
+            tile_spacing: TypedSize2D::zero(),
+            image_rendering: ImageRendering::Auto,
+            alpha_type: AlphaType::PremultipliedAlpha,
+            color: ColorF::WHITE,
+        })
+    }
+}
+
+impl RenderItem for Border {
+    // TODO: border-radius
+    // TODO: border widths + colors + styles (actual border)
+    fn render(&self) -> SpecificDisplayItem {
+        // TODO: widths
+        let top = 0.;
+        let right = 0.;
+        let bottom = 0.;
+        let left = 0.;
+        let widths = TypedSideOffsets2D::new(top, right, bottom, left);
+
+        // TODO: colors + styles
+        let details = BorderDetails::Normal(NormalBorder {
+            top: BorderSide {
+                color: ColorF::new(0., 0., 0., 0.),
+                style: BorderStyle::Solid,
+            },
+            right: BorderSide {
+                color: ColorF::new(0., 0., 0., 0.),
+                style: BorderStyle::Solid,
+            },
+            bottom: BorderSide {
+                color: ColorF::new(0., 0., 0., 0.),
+                style: BorderStyle::Solid,
+            },
+            left: BorderSide {
+                color: ColorF::new(0., 0., 0., 0.),
+                style: BorderStyle::Solid,
+            },
+            // TODO
+            radius: BorderRadius::uniform(5.0),
+            do_aa: true,
+        });
+
+        SpecificDisplayItem::Border(BorderDisplayItem { widths, details })
+    }
+}
+
+impl Into<ColorF> for Color {
+    fn into(self) -> ColorF {
+        let Color(r, g, b, a) = self;
+        ColorU::new(r, g, b, a).into()
+    }
+}
