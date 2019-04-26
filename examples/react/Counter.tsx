c7074f57c78feed875c54bc654d0e1d7df3ede45
Kamil Tomšík <info@tomsik.cz>
2019-04-26 22:55:40 +0200

    keep counter example simple

--- examples/react/Counter.tsx
+++ examples/react/Counter.tsx
@@ -7,7 +7,6 @@ export function Counter() {
 
   return (
     <View>
-      <View style={{ width: count * 10, height: 20, backgroundColor: '#cccccc' }} />
       <Text>{count}</Text>
       <Button title="++" onPress={() => setCount(count + 1)} />
     </View>
