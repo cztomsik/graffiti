6aa4ed8ec0cda0148599d80e8cb4a511e8e70ff1
Kamil Tomšík <info@tomsik.cz>
2019-10-26 20:10:21 +0200

    make createWindow & register.ts the only public api

--- src/register.ts
+++ src/register.ts
@@ -1,11 +1,14 @@
-import { getApp } from '../src'
+import { createWindow, _requestAnimationFrame } from './core/App'
 
 // setup global env for a single window
 // for a lot of apps, this will be enough
 
-global['window'] = getApp().createWindow()
+global['window'] = createWindow()
 global['document'] = window.document
 
 // TODO: leave a comment what is needed for which framework
 global['self'] = window
 global['navigator'] = window.navigator
+
+// TODO: global is stable but it's not yet clear if it should be shared or per-window
+global['requestAnimationFrame'] = _requestAnimationFrame
