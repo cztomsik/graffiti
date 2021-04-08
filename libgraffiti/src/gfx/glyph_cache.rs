375da36ee185962a98e0476e48a1d0f81f2b73a3
Kamil Tomšík <info@tomsik.cz>
2021-04-09 00:56:44 +0200

    basic GlyphCache

--- libgraffiti/src/gfx/glyph_cache.rs
+++ libgraffiti/src/gfx/glyph_cache.rs
@@ -1,4 +1,6 @@
 use super::{Atlas, Font, Glyph, TexData, Vec2, AABB, SANS_SERIF_FONT};
+use std::collections::HashMap;
+use std::hash::Hash;
 
 pub struct CachedGlyph {
     pub rect: AABB,
@@ -6,32 +8,51 @@ pub struct CachedGlyph {
 }
 
 pub struct GlyphCache {
-    // TODO: HashMap<Key, CachedGlyph>
+    glyphs: HashMap<CacheKey, CachedGlyph>,
     atlas: Atlas,
 }
 
 impl GlyphCache {
     pub fn new() -> Self {
-        Self { atlas: Atlas::new() }
+        Self {
+            glyphs: HashMap::new(),
+            atlas: Atlas::new(),
+        }
     }
 
-    pub fn use_glyph(&mut self, glyph: Glyph) -> CachedGlyph {
-        // TODO: actual caching/atlasing
-        if let Some(g) = SANS_SERIF_FONT.outline_glyph(glyph) {
-            let rect = g.px_bounds();
-            let rect = AABB::new(Vec2::new(rect.min.x, rect.min.y), Vec2::new(rect.max.x, rect.max.y));
+    pub fn use_glyph(&mut self, glyph: Glyph) -> &CachedGlyph {
+        self.glyphs.entry(CacheKey::new(&glyph)).or_insert_with(move || {
+            // TODO: evicting
+            if let Some(g) = SANS_SERIF_FONT.outline_glyph(glyph) {
+                let rect = g.px_bounds();
+                let rect = AABB::new(Vec2::new(rect.min.x, rect.min.y), Vec2::new(rect.max.x, rect.max.y));
+                let uv = AABB::ZERO;
 
-            return CachedGlyph { rect, uv: AABB::ZERO };
-        }
+                g.draw(|x, y, a| {
+                    println!("TODO: atlas draw {:?}", (x, y, a));
+                });
 
-        // unlikely, layout should skip empty & unknown glyphs
-        CachedGlyph {
-            rect: AABB::ZERO,
-            uv: AABB::ZERO,
-        }
+                return CachedGlyph { rect, uv };
+            }
+
+            // unlikely, layout should skip empty & unknown glyphs
+            CachedGlyph {
+                rect: AABB::ZERO,
+                uv: AABB::ZERO,
+            }
+        })
     }
 
     pub fn tex_data(&self) -> &TexData {
         self.atlas.tex_data()
+    }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+struct CacheKey(u16, u32);
+
+impl CacheKey {
+    pub fn new(glyph: &Glyph) -> Self {
+        Self(glyph.id.0, glyph.scale.x.to_bits())
     }
 }
