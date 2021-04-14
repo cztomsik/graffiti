e5c08e1e3ccf3ab2b24856abe738c75a3fa82f96
Kamil Tomšík <info@tomsik.cz>
2021-04-14 22:00:26 +0200

    Atom::from(&Borrow) (should avoid few allocs)

--- libgraffiti/src/util/atom.rs
+++ libgraffiti/src/util/atom.rs
@@ -1,32 +1,36 @@
+// super-simple, unsync value interning
 // TODO: NonZeroU32 (so more selector parts could be stored inline)
 
-use once_cell::sync::Lazy;
 use std::any::Any;
+use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::collections::HashSet;
+use std::fmt::{Display, Error, Formatter};
 use std::hash::{Hash, Hasher};
 use std::ops::Deref;
 use std::rc::Rc;
 
-#[derive(Debug, Clone, Eq)]
+#[derive(Debug, Eq)]
 pub struct Atom<T: Eq + Hash + 'static>(Rc<T>);
 
 // TODO: per-type static wouldn't work, rust
 //       accepts the syntax but it's shared for all types (WTF)
 //       https://github.com/rust-lang/rust/issues/22991
 thread_local! {
-    static ATOM_STORES: Lazy<RefCell<Vec<Box<dyn Any>>>> = Default::default();
+    static ATOM_STORES: RefCell<Vec<Box<dyn Any>>> = Default::default();
 }
 
 impl<T: Eq + Hash + 'static> Atom<T> {
     fn with_atoms<F, R>(f: F) -> R
     where
         T: 'static,
-        F: FnOnce(&mut HashSet<Rc<T>>) -> R,
+        F: FnOnce(&mut HashSet<Atom<T>>) -> R,
     {
         ATOM_STORES.with(|stores| {
-            if let Some(atoms) = stores.borrow_mut().iter_mut().find_map(|any| any.downcast_mut()) {
-                return f(atoms);
+            for any in &mut *stores.borrow_mut() {
+                if let Some(atoms) = any.downcast_mut() {
+                    return f(atoms);
+                }
             }
 
             let mut atoms = HashSet::new();
@@ -39,26 +43,42 @@ impl<T: Eq + Hash + 'static> Atom<T> {
     }
 }
 
-impl<T: 'static + Eq + Hash> From<T> for Atom<T> {
-    fn from(v: T) -> Self {
+impl<'a, T, Q> From<&'a Q> for Atom<T>
+where
+    T: 'static + Eq + Hash + Borrow<Q> + From<&'a Q>,
+    Q: ?Sized + Eq + Hash,
+    Atom<T>: Borrow<Q>,
+{
+    fn from(v: &'a Q) -> Self {
         Self::with_atoms(|atoms| {
-            if let Some(rc) = atoms.get(&v) {
-                return Self(rc.clone());
+            if let Some(atom) = atoms.get(&v) {
+                return atom.clone();
             }
 
-            let rc = Rc::new(v);
-            atoms.insert(Rc::clone(&rc));
+            let atom = Self(Rc::new(T::from(v)));
+            atoms.insert(atom.clone());
 
-            Self(rc)
+            atom
         })
     }
 }
 
-// conv helper
-// TODO: ToOwned vs. conflicts?
-impl From<&str> for Atom<String> {
-    fn from(v: &str) -> Self {
-        Self::from(v.to_owned())
+// derive caused some weird type errors, I think it requires T: Clone which is wrong
+impl<T: Eq + Hash> Clone for Atom<T> {
+    fn clone(&self) -> Self {
+        Self(Rc::clone(&self.0))
+    }
+}
+
+impl<T: Eq + Hash> Borrow<T> for Atom<T> {
+    fn borrow(&self) -> &T {
+        &self.0
+    }
+}
+
+impl Borrow<str> for Atom<String> {
+    fn borrow(&self) -> &str {
+        &self.0
     }
 }
 
@@ -72,9 +92,10 @@ impl<T: Eq + Hash> Deref for Atom<T> {
 
 impl<T: Eq + Hash + 'static> Drop for Atom<T> {
     fn drop(&mut self) {
-        // the one which is dropped + shared dashmap
+        // the one which is dropped + shared in hashmap
         if Rc::strong_count(&self.0) == 2 {
-            Self::with_atoms(|atoms| atoms.remove(&self.0));
+            // take() so it's dropped when out of borrow_mut()
+            Self::with_atoms(|atoms| atoms.take(self));
         }
     }
 }
@@ -88,5 +109,24 @@ impl<T: Eq + Hash> Hash for Atom<T> {
 impl<T: Eq + Hash> PartialEq for Atom<T> {
     fn eq(&self, other: &Self) -> bool {
         Rc::as_ptr(&self.0) == Rc::as_ptr(&other.0)
+    }
+}
+
+impl<T: Display + Eq + Hash> Display for Atom<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_borrowed() {
+        let _: Atom<String> = Atom::from("foo");
+
+        let foo = Atom::from("foo");
+        assert!(std::any::Any::is::<Atom<String>>(&foo));
     }
 }
