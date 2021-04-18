003fd28343d33a42dc167b5d45efa533c135cd41
Kamil Tomšík <info@tomsik.cz>
2021-04-18 12:09:10 +0200

    WIP: render works (but it's still rather PoC)

--- libgraffiti/src/renderer.rs
+++ libgraffiti/src/renderer.rs
@@ -1,22 +1,62 @@
-use crate::gfx::{Canvas, Frame, Text, TextStyle, Vec2, AABB};
+use crate::css::Style;
+use crate::gfx::{Canvas, Frame, Text, Vec2, AABB, RGBA8};
+use crate::layout::LayoutNode;
+use crate::util::SlotMap;
 use crate::{Document, NodeId, NodeType};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct RenderStyle {
+    pub hidden: bool,
+    pub bg_color: Option<RGBA8>,
+    pub outline: Option<(f32, RGBA8)>,
+}
+
+impl RenderStyle {
+    pub const DEFAULT: Self = Self {
+        hidden: false,
+        bg_color: None,
+        outline: None,
+    };
+}
 
 pub struct Renderer {
+    document: Rc<RefCell<Document>>,
+    layout_nodes: Rc<RefCell<SlotMap<NodeId, LayoutNode>>>,
+    styles: Rc<RefCell<SlotMap<NodeId, Style>>>,
+    texts: Rc<RefCell<SlotMap<NodeId, Text>>>,
     canvas: Canvas,
 }
 
 impl Renderer {
-    pub fn new() -> Self {
-        Self { canvas: Canvas::new() }
+    pub fn new(
+        document: &Rc<RefCell<Document>>,
+        layout_nodes: &Rc<RefCell<SlotMap<NodeId, LayoutNode>>>,
+        styles: &Rc<RefCell<SlotMap<NodeId, Style>>>,
+        texts: &Rc<RefCell<SlotMap<NodeId, Text>>>,
+    ) -> Self {
+        Self {
+            document: Rc::clone(&document),
+            layout_nodes: Rc::clone(&layout_nodes),
+            styles: Rc::clone(&styles),
+            texts: Rc::clone(&texts),
+            canvas: Canvas::new(),
+        }
     }
 
-    pub fn render(&mut self, document: &Document, layout_fn: &dyn Fn(NodeId) -> AABB) -> Frame {
+    pub fn render<'a>(&'a mut self) -> Frame {
+        let document = &*self.document.borrow();
+        let layout_nodes = &*self.layout_nodes.borrow();
+        let styles = &*self.styles.borrow();
+        let texts = &*self.texts.borrow();
         let root = document.root();
 
         let mut ctx = RenderContext {
             document,
             canvas: &mut self.canvas,
-            layout_fn,
+            layout_nodes,
+            styles,
+            texts,
         };
 
         ctx.render_node(Vec2::ZERO, root);
@@ -27,13 +67,18 @@ impl Renderer {
 
 struct RenderContext<'a> {
     document: &'a Document,
+    layout_nodes: &'a SlotMap<NodeId, LayoutNode>,
+    styles: &'a SlotMap<NodeId, Style>,
+    texts: &'a SlotMap<NodeId, Text>,
     canvas: &'a mut Canvas,
-    layout_fn: &'a dyn Fn(NodeId) -> AABB,
 }
 
 impl<'a> RenderContext<'a> {
     fn render_node(&mut self, offset: Vec2, node: NodeId) {
-        let rect = (self.layout_fn)(node) + offset;
+        let ln = &self.layout_nodes[node];
+        let min = offset + ln.offset().into();
+        let max = min + ln.size().into();
+        let rect = AABB::new(min, max);
 
         if self.document.node_type(node) == NodeType::Document {
             for ch in self.document.children(node) {
@@ -42,27 +87,87 @@ impl<'a> RenderContext<'a> {
         }
 
         if self.document.node_type(node) == NodeType::Element {
-            self.render_element(rect, self.document.child_nodes(node));
+            self.render_element(rect, &style(&self.styles[node]), self.document.child_nodes(node));
         }
 
         if self.document.node_type(node) == NodeType::Text {
-            self.render_text_node(rect, self.document.cdata(node));
+            // TODO: color
+            self.canvas.fill_text(&self.texts[node], rect, [0, 0, 0, 255]);
         }
     }
 
-    fn render_element(&mut self, rect: AABB, /*style: &ResolvedStyle,*/ children: impl Iterator<Item = NodeId>) {
-        self.canvas.set_fill_color([255, 63, 63, 32]);
-        self.canvas.fill_rect(rect);
+    fn render_element(&mut self, rect: AABB, style: &RenderStyle, children: impl Iterator<Item = NodeId>) {
+        if style.hidden {
+            return
+        }
+
+        // TODO: border_radius, clip, scroll, opacity, transform, ...
+
+        // TODO: outline shadow(s)
+
+        if let Some((width, color)) = style.outline {
+            self.render_outline(rect, width, color);
+        }
+
+        if let Some(bg_color) = style.bg_color {
+            self.canvas.fill_rect(rect, bg_color);
+        }
+
+        // TODO: image(s)
+
+        // TODO: inset shadow(s)
 
         for ch in children {
             self.render_node(rect.min, ch);
         }
+
+        // TODO: border
     }
 
-    fn render_text_node(&mut self, rect: AABB, text: &str) {
-        let text = Text::new(text, &TextStyle::DEFAULT);
+    fn render_outline(&mut self, rect: AABB, width: f32, color: RGBA8) {
+        let AABB { min, max } = rect;
 
-        self.canvas.set_fill_color([0, 0, 0, 255]);
-        self.canvas.fill_text(&text, rect);
+        // top
+        self.canvas.fill_rect(
+            AABB::new(min - Vec2::new(width, width), Vec2::new(max.x + width, min.y)),
+            color,
+        );
+
+        // right
+        self.canvas.fill_rect(
+            AABB::new(Vec2::new(max.x, min.y), Vec2::new(max.x + width, max.y)),
+            color,
+        );
+
+        // bottom
+        self.canvas.fill_rect(
+            AABB::new(Vec2::new(min.x - width, max.y + width), max + Vec2::new(width, width)),
+            color,
+        );
+
+        // left
+        self.canvas.fill_rect(
+            AABB::new(Vec2::new(min.x - width, min.y), Vec2::new(min.x, max.y)),
+            color,
+        );
     }
+}
+
+fn style(style: &Style) -> RenderStyle {
+    use super::css::*;
+
+    let mut res = RenderStyle::DEFAULT;
+
+    for p in style.props() {
+        use CssValue::Specified as S;
+        use StyleProp as P;
+
+        match p {
+            P::Display(S(CssDisplay::None)) => res.hidden = true,
+            P::BackgroundColor(S(c)) => res.bg_color = Some([c.r, c.g, c.b, c.a]),
+            _ => {}
+        }
+    }
+
+    res
 }
