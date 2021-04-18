bc356192fff16e01e5862751deb22f7d0696ccc1
Kamil Tomšík <info@tomsik.cz>
2021-04-18 14:36:20 +0200

    more conditionals

--- libgraffiti/src/webview.rs
+++ libgraffiti/src/webview.rs
@@ -5,10 +5,13 @@
 extern "C" {}
 
 use crate::{App, Window};
-use objc::{class, msg_send, rc::StrongPtr, runtime::Object, sel, sel_impl};
 use std::os::raw::c_void;
 use std::ptr::null;
 use std::rc::Rc;
+
+#[cfg(target_os = "macos")]
+use objc::{class, msg_send, rc::StrongPtr, runtime::Object, sel, sel_impl};
+
 #[allow(non_camel_case_types)]
 type id = *mut Object;
 
@@ -20,13 +23,7 @@ pub struct WebView {
 
 impl WebView {
     pub fn new(app: &Rc<App>) -> Self {
-        if !cfg!(target_os = "macos") {
-            #[cfg(not(target_os = "macos"))]
-            return Self {
-                _app: Rc::clone(app)
-            }
-        }
-
+        #[cfg(target_os = "macos")]
         unsafe {
             let cfg: id = msg_send![class!(WKWebViewConfiguration), new];
             let del: id = msg_send![class!(NSObject), alloc];
@@ -34,11 +31,14 @@ impl WebView {
             let () = msg_send![webview, initWithFrame:[0f64, 0., 0., 0.] configuration:cfg];
             let () = msg_send![webview, setUIDelegate: del];
 
-            Self {
+            return Self {
                 _app: Rc::clone(app),
                 webview: StrongPtr::retain(webview),
-            }
+            };
         }
+
+        #[cfg(not(target_os = "macos"))]
+        Self { _app: Rc::clone(app) };
     }
 
     pub fn attach(&mut self, window: &mut Window) {
