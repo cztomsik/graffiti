c36ff3bed68c6044d3e13fc9af44ac9a4f55453d
Kamil Tomšík <info@tomsik.cz>
2021-04-20 23:30:54 +0200

    fix atom vs borrow hash

--- libgraffiti/src/util/atom.rs
+++ libgraffiti/src/util/atom.rs
@@ -11,7 +11,12 @@ use std::ops::Deref;
 use std::rc::Rc;
 
 #[derive(Debug, Eq)]
-pub struct Atom<T: Eq + Hash + 'static>(Rc<T>);
+pub struct Atom<T: Eq + Hash + 'static>(Wrap<T>);
+
+// pub because of trait bounds for From<>
+#[doc(hidden)]
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Wrap<T>(Rc<T>);
 
 // TODO: per-type static wouldn't work, rust
 //       accepts the syntax but it's shared for all types (WTF)
@@ -21,23 +26,20 @@ thread_local! {
 }
 
 impl<T: Eq + Hash + 'static> Atom<T> {
-    fn with_atoms<F, R>(f: F) -> R
+    fn with_wraps<F, R>(f: F) -> R
     where
         T: 'static,
-        F: FnOnce(&mut HashSet<Atom<T>>) -> R,
+        F: FnOnce(&mut HashSet<Wrap<T>>) -> R,
     {
         ATOM_STORES.with(|stores| {
             for any in &mut *stores.borrow_mut() {
-                if let Some(atoms) = any.downcast_mut() {
-                    return f(atoms);
+                if let Some(wraps) = any.downcast_mut() {
+                    return f(wraps);
                 }
             }
-
-            let mut atoms = HashSet::new();
-            let res = f(&mut atoms);
-
-            stores.borrow_mut().push(Box::new(atoms));
-
+            let mut wraps = HashSet::new();
+            let res = f(&mut wraps);
+            stores.borrow_mut().push(Box::new(wraps));
             res
         })
     }
@@ -47,18 +49,17 @@ impl<'a, T, Q> From<&'a Q> for Atom<T>
 where
     T: 'static + Eq + Hash + Borrow<Q> + From<&'a Q>,
     Q: ?Sized + Eq + Hash,
-    Atom<T>: Borrow<Q>,
+    Wrap<T>: Borrow<Q>,
 {
     fn from(v: &'a Q) -> Self {
-        Self::with_atoms(|atoms| {
-            if let Some(atom) = atoms.get(&v) {
-                return atom.clone();
+        Self::with_wraps(|wraps| {
+            // beware that Hash for Atom<> is different than the one here
+            if let Some(wrap) = wraps.get(&v) {
+                return Self(Wrap(wrap.0.clone()));
             }
-
-            let atom = Self(Rc::new(T::from(v)));
-            atoms.insert(atom.clone());
-
-            atom
+            let wrap = Wrap(Rc::new(T::from(v)));
+            wraps.insert(Wrap(wrap.0.clone()));
+            Self(wrap)
         })
     }
 }
@@ -66,17 +67,17 @@ where
 // derive caused some weird type errors, I think it requires T: Clone which is wrong
 impl<T: Eq + Hash> Clone for Atom<T> {
     fn clone(&self) -> Self {
-        Self(Rc::clone(&self.0))
+        Self(Wrap(Rc::clone(&self.0 .0)))
     }
 }
 
-impl<T: Eq + Hash> Borrow<T> for Atom<T> {
+impl<T: Eq + Hash> Borrow<T> for Wrap<T> {
     fn borrow(&self) -> &T {
         &self.0
     }
 }
 
-impl Borrow<str> for Atom<String> {
+impl Borrow<str> for Wrap<String> {
     fn borrow(&self) -> &str {
         &self.0
     }
@@ -86,41 +87,49 @@ impl<T: Eq + Hash> Deref for Atom<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        self.0.as_ref()
+        self.0 .0.as_ref()
     }
 }
 
 impl<T: Eq + Hash + 'static> Drop for Atom<T> {
     fn drop(&mut self) {
-        // the one which is dropped + shared in hashmap
-        if Rc::strong_count(&self.0) == 2 {
-            // take() so it's dropped when out of borrow_mut()
-            Self::with_atoms(|atoms| atoms.take(self));
+        // the one which is dropped + one shared in hashset
+        if Rc::strong_count(&self.0 .0) == 2 {
+            Self::with_wraps(|wraps| wraps.remove(&self.0));
         }
     }
 }
 
 impl<T: Eq + Hash> Hash for Atom<T> {
     fn hash<H: Hasher>(&self, hasher: &mut H) {
-        Rc::as_ptr(&self.0).hash(hasher)
+        Rc::as_ptr(&self.0 .0).hash(hasher)
     }
 }
 
 impl<T: Eq + Hash> PartialEq for Atom<T> {
     fn eq(&self, other: &Self) -> bool {
-        Rc::as_ptr(&self.0) == Rc::as_ptr(&other.0)
+        Rc::as_ptr(&self.0 .0) == Rc::as_ptr(&other.0 .0)
     }
 }
 
 impl<T: Display + Eq + Hash> Display for Atom<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        write!(f, "{}", self.0)
+        write!(f, "{}", self.0 .0)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn eq() {
+        let a: Atom<String> = Atom::from("foo");
+        let b: Atom<String> = Atom::from(&"foo".to_owned());
+
+        assert_eq!(a, b);
+        assert_eq!(a.0 .0.as_ptr(), b.0 .0.as_ptr());
+    }
 
     #[test]
     fn from_borrowed() {
