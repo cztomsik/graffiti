c93da17464d1e986b31cab210f2bbdb45a93c142
Kamil Tomšík <info@tomsik.cz>
2021-08-18 00:07:02 +0200

    WIP: move .style from HTMLElement & SVGElement to Element

--- src/dom/HTMLElement.ts
+++ src/dom/HTMLElement.ts
@@ -1,21 +1,7 @@
 import { FocusEvent, MouseEvent } from '../events/index'
 import { Element } from './index'
-import { CSSStyleDeclaration } from '../css/CSSStyleDeclaration'
-import { setElementStyleProp } from './Document'
 
 export abstract class HTMLElement extends Element implements globalThis.HTMLElement {
-  #style
-
-  get style() {
-    if (this.#style === undefined) {
-      this.#style = new CSSStyleDeclaration(null, (prop, value) =>
-        setElementStyleProp(this.ownerDocument, this, prop, value)
-      )
-    }
-
-    return this.#style
-  }
-
   get tagName() {
     return this.localName.toUpperCase()
   }
