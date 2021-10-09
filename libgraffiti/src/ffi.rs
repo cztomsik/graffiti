4260cc728e5a8849f9ad72e31273a7397ec99e77
Kamil Tomšík <info@tomsik.cz>
2021-10-09 11:23:38 +0200

    WIP: ffi Rc<dyn Any> -> Value, make TLS more explicit

--- libgraffiti/src/ffi.rs
+++ libgraffiti/src/ffi.rs
@@ -1,72 +1,76 @@
 // thread-safe FFI
 // - meant to be safe rather than efficient, it is expected to be used with some kind of vdom
 //   to reduce number of calls
-// - the main idea here is that we hold all objects in a thread-local list of Rc<dyn Any>
+// - the main idea here is that we hold all objects in a thread-local list of values
 //   and we only return/accept indices to that list so we can avoid (unsafe) pointers
 // - each Ref you get has to be dropped (once), even if you are just traversing the tree
-// - two Refs can point to the same object/resource but it's possible to get key which is
-//   guaranteed to be same (it's pointer-based but not a pointer itself)
+// - two Refs can technically point to the same Rc<>
 
 // https://github.com/eqrion/cbindgen/issues/385
 #![allow(bare_trait_objects)]
 
-use crate::css::CssStyleDeclaration;
 use crate::util::SlotMap;
-use crate::{App, CharacterData, Document, Element, Event, Node, NodeType, Viewport, WebView, Window};
+use crate::{
+    App, CharacterData, CssStyleDeclaration, Document, Element, Event, Node, NodeId, NodeType, Viewport, WebView,
+    Window,
+};
 use crossbeam_channel::Receiver;
 use once_cell::sync::Lazy;
-use std::any::{Any, TypeId};
+use std::any::Any;
 use std::cell::RefCell;
-use std::ffi::{CStr, CString};
+use std::ffi::CStr;
 use std::marker::PhantomData;
-use std::ops::BitXor;
+use std::num::NonZeroU32;
+use std::ops::Index;
 use std::os::raw::{c_char, c_double, c_int, c_uint, c_void};
 use std::rc::Rc;
 use std::sync::RwLock;
 
 #[repr(transparent)]
-pub struct Ref<T: ?Sized>(c_uint, PhantomData<T>);
+pub struct Ref<T: ?Sized>(NonZeroU32, PhantomData<*const T>);
 
-impl<T: ?Sized> Ref<T> {
-    pub const NULL: Self = Ref(0, PhantomData);
+#[derive(Debug, Clone)]
+pub enum Value {
+    Node(Node),
+    Rc(Rc<dyn Any>),
+    String(String),
+    Vec(Vec<Value>),
 }
 
 thread_local! {
-    static REFS: Rc<RefCell<SlotMap<c_uint, Rc<dyn Any>>>> = {
-        let refs: Rc<RefCell<SlotMap<c_uint, Rc<dyn Any>>>> = Default::default();
-        let null = refs.borrow_mut().insert(Rc::new(()));
-        assert_eq!(null, Ref::<dyn Any>::NULL.0);
-        refs
-    }
+    static REFS: RefCell<SlotMap<c_uint, Value>> = Default::default();
 }
 
-// shouldn't block unless when a window is created/destroyed
+// should only block when window is being created/destroyed
 static EVENTS: Lazy<RwLock<SlotMap<u32, Receiver<Event>>>> = Lazy::new(Default::default);
 
 #[no_mangle]
 pub extern "C" fn gft_Ref_drop(obj: Ref<Any>) {
-    REFS.with(|refs| refs.borrow_mut().remove(obj.0));
+    with_tls(|tls| tls.remove(obj.0.get() - 1));
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Ref_key(obj: Ref<Any>) -> u64 {
-    let ptr: u64 = REFS.with(|refs| Rc::as_ptr(&refs.borrow()[obj.0])) as *const c_void as usize as _;
-    let key = ptr.bitxor(0xF0F0F0);
-
-    // TODO: https://github.com/denoland/deno/issues/12212
-    assert!(key <= 9007199254740991);
-
-    key
+pub extern "C" fn gft_Vec_len(vec: Ref<Vec<Value>>) -> c_uint {
+    with_tls(|tls| tls[&vec].len()) as _
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Vec_len(vec: Ref<Vec<Rc<Any>>>) -> c_uint {
-    get(vec).len() as u32
+pub extern "C" fn gft_Vec_get(vec: Ref<Vec<Value>>, index: c_uint) -> Ref<Value> {
+    let val = with_tls(|tls| tls[&vec].get(index as usize).expect("out of bounds").clone());
+    new_ref(val)
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Vec_get(vec: Ref<Vec<Rc<Any>>>, index: c_uint) -> Ref<Any> {
-    new_ref(Rc::clone(get(vec).get(index as usize).expect("out of bounds")))
+pub extern "C" fn gft_String_bytes_len(string: Ref<String>) -> c_uint {
+    with_tls(|tls| tls[&string].bytes().len() as _)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn gft_String_copy(string: Ref<String>, dest_buf: *mut u8) {
+    with_tls(|tls| {
+        let bytes = tls[&string].as_bytes();
+        dest_buf.copy_from(bytes.as_ptr(), bytes.len())
+    })
 }
 
 #[no_mangle]
@@ -75,13 +79,13 @@ pub unsafe extern "C" fn gft_App_init() -> Ref<App> {
 }
 
 #[no_mangle]
-pub extern "C" fn gft_App_current() -> Ref<App> {
-    App::current().into()
+pub extern "C" fn gft_App_current() -> Option<Ref<App>> {
+    App::current().map(Ref::from)
 }
 
 #[no_mangle]
 pub extern "C" fn gft_App_tick(app: Ref<App>) {
-    get(app).tick()
+    with_tls(|tls| tls[&app].tick())
 }
 
 #[no_mangle]
@@ -104,63 +108,63 @@ pub unsafe extern "C" fn gft_Window_new(title: *const c_char, width: c_int, heig
 // Window_next_event: |w| EVENTS.read().unwrap()[w].try_recv().ok().map(event),
 
 #[no_mangle]
-pub extern "C" fn gft_Window_title(win: Ref<Window>) -> *mut c_char {
-    CString::new(get(win).title()).unwrap().into_raw()
+pub extern "C" fn gft_Window_title(win: Ref<Window>) -> Ref<String> {
+    with_tls(|tls| tls[&win].title().into())
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_Window_set_title(win: Ref<Window>, title: *const c_char) {
-    get(win).set_title(to_str(title))
+pub unsafe extern "C" fn gft_Window_set_title(win: Ref<Window>, title: *const c_char, title_len: u32) {
+    with_tls(|tls| tls[&win].set_title(to_str(title, title_len)))
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_width(win: Ref<Window>) -> c_int {
-    get(win).size().0
+    with_tls(|tls| tls[&win].size().0)
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_height(win: Ref<Window>) -> c_int {
-    get(win).size().1
+    with_tls(|tls| tls[&win].size().1)
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_resize(win: Ref<Window>, width: c_int, height: c_int) {
-    get(win).set_size((width, height))
+    with_tls(|tls| tls[&win].set_size((width, height)))
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_should_close(win: Ref<Window>) -> bool {
-    get(win).should_close()
+    with_tls(|tls| tls[&win].should_close())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_show(win: Ref<Window>) {
-    get(win).show()
+    with_tls(|tls| tls[&win].show())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_hide(win: Ref<Window>) {
-    get(win).hide()
+    with_tls(|tls| tls[&win].hide())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_focus(win: Ref<Window>) {
-    get(win).focus()
+    with_tls(|tls| tls[&win].focus())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_minimize(win: Ref<Window>) {
-    get(win).minimize()
+    with_tls(|tls| tls[&win].minimize())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_maximize(win: Ref<Window>) {
-    get(win).maximize()
+    with_tls(|tls| tls[&win].maximize())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Window_restore(win: Ref<Window>) {
-    get(win).restore()
+    with_tls(|tls| tls[&win].restore())
 }
 
 #[no_mangle]
@@ -170,22 +174,22 @@ pub extern "C" fn gft_WebView_new() -> Ref<WebView> {
 
 #[no_mangle]
 pub extern "C" fn gft_WebView_attach(webview: Ref<WebView>, win: Ref<Window>) {
-    get(webview).attach(&get(win))
+    with_tls(|tls| tls[&webview].attach(&tls[&win]))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_WebView_load_url(webview: Ref<WebView>, url: *const c_char) {
-    get(webview).load_url(to_str(url))
+pub unsafe extern "C" fn gft_WebView_load_url(webview: Ref<WebView>, url: *const c_char, url_len: u32) {
+    with_tls(|tls| tls[&webview].load_url(to_str(url, url_len)))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_WebView_eval(webview: Ref<WebView>, script: *const c_char) {
-    get(webview).eval(to_str(script))
+pub unsafe extern "C" fn gft_WebView_eval(webview: Ref<WebView>, script: *const c_char, script_len: u32) {
+    with_tls(|tls| tls[&webview].eval(to_str(script, script_len)))
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Document_new() -> Ref<Document> {
-    Document::new().into()
+    Document::new().as_node().into()
 }
 
 #[no_mangle]
@@ -199,53 +203,55 @@ pub unsafe extern "C" fn gft_Document_create_text_node(doc: Ref<Document>, data:
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_Document_create_comment(doc: Ref<Document>, data: *const c_char) -> Ref<CharacterData> {
-    get(doc).create_comment(to_str(data)).into()
+
+#[no_mangle]
+pub extern "C" fn gft_Node_id(node: Ref<Node>) -> NodeId {
+    with_tls(|tls| tls[&node].id())
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Node_node_type(node: Ref<Node>) -> NodeType {
-    get_node(node).node_type()
+    with_tls(|tls| tls[&node].node_type())
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Node_parent_node(node: Ref<Node>) -> Ref<Node> {
-    get_node(node).parent_node().into()
+pub extern "C" fn gft_Node_parent_node(node: Ref<Node>) -> Option<Ref<Node>> {
+    with_tls(|tls| tls[&node].parent_node()).map(Ref::from)
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Node_first_child(node: Ref<Node>) -> Ref<Node> {
-    get_node(node).first_child().into()
+pub extern "C" fn gft_Node_first_child(node: Ref<Node>) -> Option<Ref<Node>> {
+    with_tls(|tls| tls[&node].first_child()).map(Ref::from)
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Node_last_child(node: Ref<Node>) -> Ref<Node> {
-    get_node(node).last_child().into()
+pub extern "C" fn gft_Node_last_child(node: Ref<Node>) -> Option<Ref<Node>> {
+    with_tls(|tls| tls[&node].last_child()).map(Ref::from)
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Node_previous_sibling(node: Ref<Node>) -> Ref<Node> {
-    get_node(node).previous_sibling().into()
+pub extern "C" fn gft_Node_previous_sibling(node: Ref<Node>) -> Option<Ref<Node>> {
+    with_tls(|tls| tls[&node].previous_sibling()).map(Ref::from)
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Node_next_sibling(node: Ref<Node>) -> Ref<Node> {
-    get_node(node).next_sibling().into()
+pub extern "C" fn gft_Node_next_sibling(node: Ref<Node>) -> Option<Ref<Node>> {
+    with_tls(|tls| tls[&node].next_sibling()).map(Ref::from)
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Node_append_child(parent: Ref<Node>, child: Ref<Node>) {
-    get_node(parent).append_child(get_node(child))
+    with_tls(|tls| tls[&parent].append_child(&tls[&child]))
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Node_insert_before(parent: Ref<Node>, child: Ref<Node>, before: Ref<Node>) {
-    get_node(parent).insert_before(get_node(child), get_node(before))
+    with_tls(|tls| tls[&parent].insert_before(&tls[&child], &tls[&before]))
 }
 
 #[no_mangle]
 pub extern "C" fn gft_Node_remove_child(parent: Ref<Node>, child: Ref<Node>) {
-    get_node(parent).remove_child(get_node(child))
+    with_tls(|tls| tls[&parent].remove_child(&tls[&child]))
 }
 
 #[no_mangle]
@@ -267,54 +273,76 @@ pub unsafe extern "C" fn gft_Node_query_selector_all(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_CharacterData_data(node: Ref<CharacterData>) -> *mut c_char {
-    CString::new(get(node).data()).unwrap().into_raw()
+pub extern "C" fn gft_CharacterData_data(node: Ref<CharacterData>) -> Ref<String> {
+    with_tls(|tls| tls[&node].data()).into()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_CharacterData_set_data(node: Ref<CharacterData>, data: *const c_char) {
-    get(node).set_data(to_str(data))
+pub unsafe extern "C" fn gft_CharacterData_set_data(node: Ref<CharacterData>, data: *const c_char, data_len: u32) {
+    with_tls(|tls| tls[&node].set_data(to_str(data, data_len)))
 }
 
 #[no_mangle]
-pub extern "C" fn gft_Element_local_name(el: Ref<Element>) -> *mut c_char {
-    CString::new(get(el).local_name().as_str()).unwrap().into_raw()
+pub extern "C" fn gft_Element_local_name(el: Ref<Element>) -> Ref<String> {
+    with_tls(|tls| tls[&el].local_name().to_string()).into()
 }
 
 // Element_attribute_names: |el| get(el).attribute_names(),
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_Element_attribute(el: Ref<Element>, att: *const c_char) -> *mut c_char {
-    match get(el).attribute(to_str(att)) {
-        Some(s) => CString::new(s).unwrap().into_raw(),
-        None => std::ptr::null_mut(),
+pub unsafe extern "C" fn gft_Element_attribute(
+    el: Ref<Element>,
+    att: *const c_char,
+    att_len: u32,
+) -> Option<Ref<String>> {
+    with_tls(|tls| tls[&el].attribute(to_str(att, att_len))).map(Ref::from)
     }
+
+#[no_mangle]
+pub unsafe extern "C" fn gft_Element_set_attribute(
+    el: Ref<Element>,
+    att: *const c_char,
+    att_len: u32,
+    val: *const c_char,
+    val_len: u32,
+) {
+    with_tls(|tls| tls[&el].set_attribute(to_str(att, att_len), to_str(val, val_len)))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_Element_set_attribute(el: Ref<Element>, att: *const c_char, val: *const c_char) {
-    get(el).set_attribute(to_str(att), to_str(val))
+pub unsafe extern "C" fn gft_Element_remove_attribute(el: Ref<Element>, att: *const c_char, att_len: u32) {
+    with_tls(|tls| tls[&el].remove_attribute(to_str(att, att_len)))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gft_Element_remove_attribute(el: Ref<Element>, att: *const c_char) {
-    get(el).remove_attribute(to_str(att))
+pub unsafe extern "C" fn gft_Element_matches(el: Ref<Element>, selector: *const c_char, selector_len: u32) -> bool {
+    with_tls(|tls| tls[&el].matches(to_str(selector, selector_len)))
 }
 
 #[no_mangle]
 pub extern "C" fn gft_CssStyleDeclaration_length(style: Ref<CssStyleDeclaration>) -> c_uint {
-    get(style).length() as _
+    with_tls(|tls| tls[&style].length() as _)
 }
 
-// CssStyleDeclaration_property_value: |style, prop: String| get(el).style_property_value(&prop),
 #[no_mangle]
-pub unsafe extern "C" fn gft_CssStyleDeclaration_set_property(
+pub unsafe extern "C" fn gft_CssStyleDeclaration_property_value(
     style: Ref<CssStyleDeclaration>,
     prop: *const c_char,
+    prop_len: u32,
+) -> Option<Ref<String>> {
+    with_tls(|tls| tls[&style].property_value(to_str(prop, prop_len)).map(Ref::from))
+}
+
+#[no_mangle]
+pub extern "C" fn gft_CssStyleDeclaration_set_property(
+    style: Ref<CssStyleDeclaration>,
+    prop: *const c_char,
+    prop_len: u32,
     val: *const c_char,
+    val_len: u32,
 ) {
-    //get(style).set_property(to_str(prop), to_str(val))
-    println!("TODO: style.set_property()")
+    todo!()
+    //with_tls(|tls| tls[&style].set_property(to_str(prop, prop_len), to_str(val, val_len)))
 }
 
 // Viewport_new: |w: f64, h: f64, doc: u32| to_id(Rc::new(Viewport::new((w as _, h as _), get(doc)))),
@@ -327,84 +355,65 @@ pub extern "C" fn gft_Viewport_resize(viewport: Ref<Viewport>, width: c_double, 
 
 // Viewport_element_from_point: |vp, x: f64, y: f64| get(vp).element_from_point((x as _, y as _))
 
-impl<T: ?Sized> From<Option<Rc<T>>> for Ref<T>
-where
-    Rc<T>: Into<Ref<T>>,
-{
-    fn from(opt: Option<Rc<T>>) -> Self {
-        match opt {
-            Some(obj) => obj.into(),
-            None => Ref::NULL,
-        }
+// TODO: make it more explicit, into() hides TLS
+impl<T: 'static> From<Node> for Ref<T> {
+    fn from(node: Node) -> Self {
+        new_ref(Value::Node(node))
     }
 }
 
 impl<T: 'static> From<Rc<T>> for Ref<T> {
     fn from(rc: Rc<T>) -> Self {
-        new_ref(rc)
+        new_ref(Value::Rc(rc))
     }
 }
 
-// TODO: add to_node() to Node trait
-impl From<Rc<dyn Node>> for Ref<dyn Node> {
-    fn from(node: Rc<dyn Node>) -> Ref<dyn Node> {
-        match node.node_type() {
-            NodeType::Document => new_ref(node.downcast::<Document>().unwrap()),
-            NodeType::Element => new_ref(node.downcast::<Element>().unwrap()),
-            NodeType::Text => new_ref(node.downcast::<CharacterData>().unwrap()),
-            NodeType::Comment => new_ref(node.downcast::<CharacterData>().unwrap()),
-            _ => unreachable!(),
+impl From<String> for Ref<String> {
+    fn from(string: String) -> Self {
+        new_ref(Value::String(string))
         }
+}
+
+impl From<Vec<Value>> for Ref<Vec<Value>> {
+    fn from(vec: Vec<Value>) -> Self {
+        new_ref(Value::Vec(vec))
     }
 }
 
-fn new_ref<T: ?Sized>(any: Rc<dyn Any>) -> Ref<T> {
-    REFS.with(|refs| Ref(refs.borrow_mut().insert(any), PhantomData))
+fn new_ref<T: ?Sized>(value: Value) -> Ref<T> {
+    REFS.with(|refs| {
+        Ref(
+            NonZeroU32::new(refs.borrow_mut().insert(value) + 1).unwrap(),
+            PhantomData,
+        )
+    })
 }
 
-fn any<T: ?Sized>(obj: Ref<T>) -> Rc<dyn Any> {
-    REFS.with(|refs| Rc::clone(&refs.borrow()[obj.0]))
+unsafe fn to_str<'a>(ptr: *const c_char, len: u32) -> &'a str {
+    // match len {
+    //     0 => CStr::from_ptr(ptr).to_str().expect("invalid string"),
+    //     len => std::str::from_utf8_unchecked(std::slice::from_raw_parts(ptr as _, len as _)),
+    // }
+    std::str::from_utf8_unchecked(std::slice::from_raw_parts(ptr as _, len as _))
 }
 
-fn get<T: 'static>(obj: Ref<T>) -> Rc<T> {
-    Rc::downcast::<T>(any(obj)).expect("invalid object type")
+fn with_tls<T>(mut fun: impl FnMut(&mut SlotMap<c_uint, Value>) -> T) -> T {
+    REFS.with(|refs| {
+        let mut refs = refs.borrow_mut();
+        fun(&mut *refs)
+    })
 }
 
-fn get_node(node: Ref<Node>) -> Rc<dyn Node> {
-    let any = any(node);
-    let type_id = (&*any).type_id();
+impl<T: 'static> Index<&Ref<T>> for SlotMap<c_uint, Value> {
+    type Output = T;
 
-    if type_id == TypeId::of::<Element>() {
-        Rc::downcast::<Element>(any).unwrap()
-    } else if type_id == TypeId::of::<CharacterData>() {
-        Rc::downcast::<CharacterData>(any).unwrap()
-    } else if type_id == TypeId::of::<Document>() {
-        Rc::downcast::<Document>(any).unwrap()
-    } else {
-        panic!("not a node")
+    fn index(&self, index: &Ref<T>) -> &T {
+        match &self[index.0.get() - 1] {
+            Value::Node(node) => node.downcast_ref::<T>(),
+            Value::Rc(rc) => rc.downcast_ref::<T>(),
+            Value::String(string) => <dyn Any>::downcast_ref(string),
+            Value::Vec(vec) => <dyn Any>::downcast_ref(vec),
     }
+        .expect("invalid object type")
 }
-
-unsafe fn to_str<'a>(ptr: *const c_char) -> &'a str {
-    CStr::from_ptr(ptr).to_str().expect("invalid string")
-}
-
-// TODO: ?
-fn event(ev: Event) -> (String, Option<(f64, f64)>, Option<u32>) {
-    let res = match ev {
-        Event::CursorPos(x, y) => ("mousemove", Some((x, y)), None),
-        Event::MouseDown => ("mousedown", None, None),
-        Event::MouseUp => ("mouseup", None, None),
-        Event::Scroll(x, y) => ("scroll", Some((x, y)), None),
-
-        // JS e.which
-        Event::KeyDown(code) => ("keydown", None, Some(code)),
-        Event::KeyUp(code) => ("keyup", None, Some(code)),
-        Event::KeyPress(ch) => ("keypress", None, Some(ch)),
-        Event::Resize(w, h) => ("resize", Some((w as _, h as _)), None),
-        Event::FramebufferSize(w, h) => ("fbsize", Some((w as _, h as _)), None),
-        Event::Close => ("close", None, None),
-    };
-
-    (res.0.to_owned(), res.1, res.2)
 }
