acf8477324a5c9be93675be646b250a68bb8f0bc
Kamil Tomšík <info@tomsik.cz>
2021-10-16 15:58:24 +0200

    update python test

--- libgraffiti/examples/hello.py
+++ libgraffiti/examples/hello.py
@@ -6,37 +6,38 @@ from ctypes import cdll, c_int, c_uint, c_char_p
 lib = cdll.LoadLibrary('../target/debug/libgraffiti.dylib')
 
 lib.gft_App_init.restype = c_uint
+lib.gft_App_tick.argtypes = [c_uint]
 
-lib.gft_Window_new.argtypes = [c_char_p, c_int, c_int]
+lib.gft_Window_new.argtypes = [c_char_p, c_uint, c_int, c_int]
 lib.gft_Window_new.restype = c_uint
-lib.gft_Window_title.argtypes = [c_uint]
-lib.gft_Window_title.restype = c_char_p
 
 lib.gft_Document_new.restype = c_uint
-lib.gft_Document_create_element.argtypes = [c_uint, c_char_p]
+lib.gft_Document_create_element.argtypes = [c_uint, c_char_p, c_uint]
 lib.gft_Document_create_element.restype = c_uint
-lib.gft_Document_create_text_node.argtypes = [c_uint, c_char_p]
+lib.gft_Document_create_text_node.argtypes = [c_uint, c_char_p, c_uint]
 lib.gft_Document_create_text_node.restype = c_uint
 lib.gft_Node_append_child.argtypes = [c_uint, c_uint]
-lib.gft_Node_query_selector.argtypes = [c_uint, c_char_p]
+lib.gft_Node_query_selector.argtypes = [c_uint, c_char_p, c_uint]
 lib.gft_Node_query_selector.restype = c_uint
+lib.gft_Node_id.argtypes = [c_uint]
+lib.gft_Node_id.restype = c_uint
 
-lib.gft_App_init()
+app = lib.gft_App_init()
 
 async def main():
-  win = lib.gft_Window_new(b'Hello', 800, 600)
+  win = lib.gft_Window_new(b'Hello', 5, 800, 600)
 
   doc = lib.gft_Document_new()
-  div = lib.gft_Document_create_element(doc, b'div')
-  hello = lib.gft_Document_create_text_node(doc, b'Hello')
+  div = lib.gft_Document_create_element(doc, b'div', 3)
+  hello = lib.gft_Document_create_text_node(doc, b'Hello', 5)
   lib.gft_Node_append_child(doc, div)
   lib.gft_Node_append_child(div, hello)
 
-  print('div: {}'.format(div))
-  print('querySelector: {}'.format(lib.gft_Node_query_selector(doc, b'div')))
+  print('div id: {}'.format(lib.gft_Node_id(div)))
+  print('querySelector: {}'.format(lib.gft_Node_id(lib.gft_Node_query_selector(doc, b'div', 3))))
 
   while True:
-    lib.gft_App_tick()
+    lib.gft_App_tick(app)
     await asyncio.sleep(0.1)
 
 loop = asyncio.get_event_loop()
