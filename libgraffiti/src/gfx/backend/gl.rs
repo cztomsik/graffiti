836f5aed0751af3a0ce2da220d3105774a0bd1b8
Kamil Tomšík <info@tomsik.cz>
2021-10-16 15:26:06 +0200

    use glow in gl backend

--- libgraffiti/src/gfx/backend/gl.rs
+++ libgraffiti/src/gfx/backend/gl.rs
@@ -2,7 +2,12 @@
 
 use super::super::{DrawOp, Frame, TexData, Vertex};
 use super::RenderBackend;
+use glow::*;
+use std::ffi::CString;
 use std::marker::PhantomData;
+use std::mem;
+use std::os::raw::c_void;
+use std::ptr;
 
 /// Super-simple OpengGL 2.1 backend
 /// - one vbo
@@ -13,10 +18,11 @@ pub struct GlBackend {
     // !Send, !Sync
     _marker: PhantomData<*mut ()>,
 
-    vao: GLuint,
-    vbo: GLuint,
-    tex: GLuint,
-    program: GLuint,
+    gl: glow::Context,
+    vao: Option<NativeVertexArray>,
+    vbo: NativeBuffer,
+    tex: NativeTexture,
+    program: NativeProgram,
 }
 
 macro_rules! offsetof {
@@ -28,63 +34,61 @@ macro_rules! offsetof {
 }
 
 impl GlBackend {
-    pub unsafe fn new(load_symbol: impl FnMut(&str) -> *mut c_void) -> Self {
-        self::load_with(load_symbol);
+    // TODO: could be safe if everything happenned in App::push_task()
+    pub unsafe fn new(load_symbol: impl FnMut(&str) -> *const c_void) -> Self {
+        let gl = glow::Context::from_loader_function(load_symbol);
 
         let STRIDE = mem::size_of::<Vertex>() as _;
 
         // not used but webgl & opengl core profile require it
-        let mut vao = 0;
-        glGenVertexArrays(1, &mut vao);
-        glBindVertexArray(vao);
-        check("vao");
+        let vao = gl.create_vertex_array().ok();
+        gl.bind_vertex_array(vao);
+        check(&gl, "vao");
 
-        glDisable(GL_DEPTH_TEST);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glBlendEquation(GL_FUNC_ADD);
+        gl.disable(DEPTH_TEST);
+        gl.enable(BLEND);
+        gl.blend_func(SRC_ALPHA, ONE_MINUS_SRC_ALPHA);
+        gl.blend_equation(FUNC_ADD);
 
         // one vbo
-        let mut vbo = 0;
-        glGenBuffers(1, &mut vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        assert_ne!(vbo, 0);
-        check("vbo");
+        let vbo = gl.create_buffer().expect("create vbo");
+        gl.bind_buffer(ARRAY_BUFFER, Some(vbo));
+        check(&gl, "vbo");
 
         // one texture
-        let mut tex = 0;
-        glGenTextures(1, &mut tex);
-        glBindTexture(GL_TEXTURE_2D, tex);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR as _);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR as _);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE as _);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE as _);
+        let tex = gl.create_texture().expect("create tex");
+        gl.bind_texture(TEXTURE_2D, Some(tex));
+        gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MIN_FILTER, LINEAR as _);
+        gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_MAG_FILTER, LINEAR as _);
+        gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_WRAP_S, CLAMP_TO_EDGE as _);
+        gl.tex_parameter_i32(TEXTURE_2D, TEXTURE_WRAP_T, CLAMP_TO_EDGE as _);
         // needed if RGB
-        //glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        check("texture");
+        //gl.pixel_store_i32(UNPACK_ALIGNMENT, 1);
+        check(&gl, "texture");
 
         // one shader
-        let program = create_program(VS, FS);
-        glUseProgram(program);
+        let program = create_program(&gl, VS, FS);
+        gl.use_program(Some(program));
 
         // setup attrs
-        let a_pos = glGetAttribLocation(program, c_str!("a_pos")) as _;
-        glEnableVertexAttribArray(a_pos);
-        glVertexAttribPointer(a_pos, 2, GL_FLOAT, GL_FALSE, STRIDE, offsetof!(Vertex.xy));
-        check("a_pos");
+        let a_pos = gl.get_attrib_location(program, "a_pos").unwrap();
+        gl.enable_vertex_attrib_array(a_pos);
+        gl.vertex_attrib_pointer_f32(a_pos, 2, FLOAT, false, STRIDE, offsetof!(Vertex.xy) as _);
+        check(&gl, "a_pos");
 
-        let a_uv = glGetAttribLocation(program, c_str!("a_uv")) as _;
-        glEnableVertexAttribArray(a_uv);
-        glVertexAttribPointer(a_uv, 2, GL_FLOAT, GL_FALSE, STRIDE, offsetof!(Vertex.uv));
-        check("a_uv");
+        let a_uv = gl.get_attrib_location(program, "a_uv").unwrap();
+        gl.enable_vertex_attrib_array(a_uv);
+        gl.vertex_attrib_pointer_f32(a_uv, 2, FLOAT, false, STRIDE, offsetof!(Vertex.uv) as _);
+        check(&gl, "a_uv");
 
-        let a_color = glGetAttribLocation(program, c_str!("a_color")) as _;
-        glEnableVertexAttribArray(a_color);
-        glVertexAttribPointer(a_color, 4, GL_UNSIGNED_BYTE, GL_TRUE, STRIDE, offsetof!(Vertex.color));
-        check("a_color");
+        let a_color = gl.get_attrib_location(program, "a_color").unwrap();
+        gl.enable_vertex_attrib_array(a_color);
+        gl.vertex_attrib_pointer_f32(a_color, 4, UNSIGNED_BYTE, true, STRIDE, offsetof!(Vertex.color) as _);
+        check(&gl, "a_color");
 
         Self {
             _marker: PhantomData,
+            gl,
             vao,
             vbo,
             tex,
@@ -96,55 +100,60 @@ impl GlBackend {
 impl Drop for GlBackend {
     fn drop(&mut self) {
         unsafe {
-            glDeleteBuffers(1, &mut self.vbo);
-            glDeleteVertexArrays(1, &mut self.vao);
-            glDeleteTextures(1, &mut self.tex);
-            glDeleteProgram(self.program);
+            if let Some(vao) = self.vao {
+                self.gl.delete_vertex_array(vao);
+            }
+
+            self.gl.delete_buffer(self.vbo);
+            self.gl.delete_texture(self.tex);
+            self.gl.delete_program(self.program);
         }
     }
 }
 
 impl RenderBackend for GlBackend {
     // TODO: unsafe because it depends on (right) context being active
-    fn render_frame(&mut self, frame: Frame) {
+    fn render_frame(&self, frame: Frame) {
         unsafe {
             // TODO: uniform
             // TODO: glViewport(0, 0, width, height);
 
-            glBufferData(
-                GL_ARRAY_BUFFER,
-                mem::size_of_val(&frame.vertices[..]) as _,
-                frame.vertices.as_ptr() as _,
-                GL_DYNAMIC_DRAW,
+            self.gl.buffer_data_u8_slice(
+                ARRAY_BUFFER,
+                std::slice::from_raw_parts(frame.vertices.as_ptr() as _, mem::size_of_val(&frame.vertices[..]) as _),
+                DYNAMIC_DRAW,
             );
-            check("upload vbo");
+            check(&self.gl, "upload vbo");
 
-            glClearColor(1., 1., 1., 1.);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            self.gl.clear_color(1., 1., 1., 1.);
+            self.gl.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
 
-            let mut index: GLint = 0;
+            let mut index: i32 = 0;
 
             for op in frame.draw_ops {
                 match op {
                     DrawOp::TexData(TexData { width, height, pixels }) => {
-                        glBindTexture(GL_TEXTURE_2D, self.tex);
-                        glTexImage2D(
-                            GL_TEXTURE_2D,
+                        self.gl.bind_texture(TEXTURE_2D, Some(self.tex));
+                        self.gl.tex_image_2d(
+                            TEXTURE_2D,
                             0,
-                            GL_RGBA as _,
+                            RGBA as _,
                             width,
                             height,
                             0,
-                            GL_RGBA,
-                            GL_UNSIGNED_BYTE,
-                            pixels.as_ptr() as _,
+                            RGBA,
+                            UNSIGNED_BYTE,
+                            Some(std::slice::from_raw_parts(
+                                pixels.as_ptr() as _,
+                                mem::size_of_val(&pixels[..]) as _,
+                            )),
                         );
-                        glActiveTexture(GL_TEXTURE0);
+                        self.gl.active_texture(TEXTURE0);
                     }
 
                     DrawOp::DrawArrays(num) => {
-                        glDrawArrays(GL_TRIANGLES, index, num as _);
-                        index += num as GLint;
+                        self.gl.draw_arrays(TRIANGLES, index, num as _);
+                        index += num as i32;
                     }
                 }
             }
@@ -192,183 +201,37 @@ void main() {
 
 // gl utils
 
-use std::ffi::CString;
-use std::mem;
-use std::ptr;
+unsafe fn create_program(gl: &glow::Context, vs: &str, fs: &str) -> NativeProgram {
+    let vs = shader(gl, VERTEX_SHADER, vs);
+    let fs = shader(gl, FRAGMENT_SHADER, fs);
 
-unsafe fn create_program(vs: &str, fs: &str) -> GLuint {
-    let vs = shader(GL_VERTEX_SHADER, vs);
-    check("vertex shader");
+    let program = gl.create_program().unwrap();
+    gl.attach_shader(program, vs);
+    gl.attach_shader(program, fs);
+    gl.link_program(program);
 
-    let fs = shader(GL_FRAGMENT_SHADER, fs);
-    check("fragment shader");
-
-    let program = glCreateProgram();
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-
-    let mut success = GL_FALSE as GLint;
-    glGetProgramiv(program, GL_LINK_STATUS, &mut success);
-
-    if success != GL_TRUE as GLint {
-        panic!("{}", get_program_info_log(program));
+    if !gl.get_program_link_status(program) {
+        panic!("{}", gl.get_program_info_log(program));
     }
 
     program
 }
 
-unsafe fn shader(shader_type: GLuint, source: &str) -> GLuint {
-    let shader = glCreateShader(shader_type);
-    glShaderSource(shader, 1, &*c_str!(source), ptr::null());
-    glCompileShader(shader);
+unsafe fn shader(gl: &glow::Context, shader_type: u32, source: &str) -> NativeShader {
+    let shader = gl.create_shader(shader_type).unwrap();
+    gl.shader_source(shader, source);
+    gl.compile_shader(shader);
 
-    let mut success = GL_FALSE as GLint;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &mut success);
-
-    if success != GL_TRUE as GLint {
-        panic!("{}", get_shader_info_log(shader));
+    if !gl.get_shader_compile_status(shader) {
+        panic!("{}", gl.get_shader_info_log(shader));
     }
 
     shader
 }
 
-unsafe fn check(hint: &str) {
-    let err = glGetError();
-    if err != GL_NO_ERROR {
+unsafe fn check(gl: &glow::Context, hint: &str) {
+    let err = gl.get_error();
+    if err != NO_ERROR {
         panic!("gl err {} near {}", err, hint);
-    }
-}
-
-unsafe fn get_shader_info_log(shader: GLuint) -> String {
-    let mut len = 0;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &mut len);
-
-    let mut buf = vec![0i8; len as usize];
-    glGetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-    buf.set_len(len as usize);
-
-    String::from_utf8_unchecked(mem::transmute(buf))
-}
-
-unsafe fn get_program_info_log(program: GLuint) -> String {
-    let mut len = 0;
-    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &mut len);
-
-    let mut buf = vec![0i8; len as usize];
-    glGetProgramInfoLog(program, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
-    buf.set_len(len as usize);
-
-    String::from_utf8_unchecked(mem::transmute(buf))
-}
-
-// hand-written opengl bindings/loader
-// - retains glXxXx() naming so it matches examples from other langs
-// - no deps, faster to compile
-// - explicit about what we really need
-use std::os::raw::{c_char, c_float, c_int, c_uchar, c_uint, c_void};
-
-// gl types
-type GLenum = c_uint;
-type GLboolean = c_uchar;
-type GLbitfield = c_uint;
-type GLint = c_int;
-type GLuint = c_uint;
-type GLsizei = c_int;
-type GLfloat = c_float;
-type GLchar = c_char;
-type GLsizeiptr = isize;
-
-// consts
-const GL_COLOR_BUFFER_BIT: GLenum = 0x00004000;
-const GL_DEPTH_BUFFER_BIT: GLenum = 0x00000100;
-//const GL_STENCIL_BUFFER_BIT: GLenum = 0x00000400;
-const GL_FALSE: GLboolean = 0;
-const GL_TRUE: GLboolean = 1;
-const GL_TRIANGLES: GLenum = 0x0004;
-const GL_SRC_ALPHA: GLenum = 0x0302;
-const GL_ONE_MINUS_SRC_ALPHA: GLenum = 0x0303;
-const GL_FUNC_ADD: GLenum = 0x8006;
-const GL_ARRAY_BUFFER: GLenum = 0x8892;
-//const GL_STREAM_DRAW: GLenum = 0x88E0;
-//const GL_STATIC_DRAW: GLenum = 0x88E4;
-const GL_DYNAMIC_DRAW: GLenum = 0x88E8;
-const GL_TEXTURE_2D: GLenum = 0x0DE1;
-const GL_BLEND: GLenum = 0x0BE2;
-const GL_DEPTH_TEST: GLenum = 0x0B71;
-const GL_GREATER: GLenum = 0x0204;
-const GL_NO_ERROR: GLenum = 0;
-const GL_UNSIGNED_BYTE: GLenum = 0x1401;
-const GL_FLOAT: GLenum = 0x1406;
-const GL_RGBA: GLenum = 0x1908;
-const GL_FRAGMENT_SHADER: GLenum = 0x8B30;
-const GL_VERTEX_SHADER: GLenum = 0x8B31;
-const GL_LINK_STATUS: GLenum = 0x8B82;
-const GL_LINEAR: GLenum = 0x2601;
-const GL_TEXTURE_MAG_FILTER: GLenum = 0x2800;
-const GL_TEXTURE_MIN_FILTER: GLenum = 0x2801;
-const GL_TEXTURE_WRAP_S: GLenum = 0x2802;
-const GL_TEXTURE_WRAP_T: GLenum = 0x2803;
-const GL_TEXTURE0: GLenum = 0x84C0;
-const GL_CLAMP_TO_EDGE: GLenum = 0x812F;
-const GL_COMPILE_STATUS: GLenum = 0x8B81;
-const GL_INFO_LOG_LENGTH: GLenum = 0x8B84;
-
-// what we need
-dylib! {
-    extern "C" {
-        // err
-        fn glGetError() -> GLenum;
-        fn glGetProgramiv(program: GLuint, pname: GLenum, params: *mut GLint);
-        fn glGetProgramInfoLog(program: GLuint, buf_size: GLsizei, len: *mut GLsizei, log: *mut GLchar);
-        fn glGetShaderiv(shader: GLuint, pname: GLenum, params: *mut GLint);
-        fn glGetShaderInfoLog(shader: GLuint, buf_size: GLsizei, len: *mut GLsizei, log: *mut GLchar);
-
-        // vao
-        fn glGenVertexArrays(n: GLsizei, arrays: *mut GLuint);
-        fn glBindVertexArray(vao: GLuint);
-        fn glDeleteVertexArrays(n: GLsizei, arrays: *mut GLuint);
-
-        // program, shaders
-        fn glCreateProgram() -> GLuint;
-        fn glUseProgram(program: GLuint);
-        fn glCreateShader(kind: GLenum) -> GLuint;
-        fn glShaderSource(shader: GLuint, count: GLsizei, source: *const *const GLchar, len: *const GLint);
-        fn glCompileShader(shader: GLuint);
-        fn glAttachShader(program: GLuint, shader: GLuint);
-        fn glGetUniformLocation(program: GLuint, name: *const GLchar) -> GLint;
-        fn glGetAttribLocation(program: GLuint, name: *const GLchar) -> GLint;
-        fn glLinkProgram(program: GLuint);
-        fn glDeleteShader(shader: GLuint);
-        fn glDeleteProgram(shader: GLuint);
-
-        // tex
-        fn glGenTextures(n: GLsizei, textures: *mut GLuint);
-        fn glBindTexture(target: GLenum, tex: GLuint);
-        fn glTexParameteri(target: GLenum, pname: GLenum, params: GLint);
-        fn glTexImage2D(target: GLenum, level: GLint, format: GLint, width: GLsizei, height: GLsizei, border: GLint, internal_format: GLenum, kind: GLenum, pixels: *const c_void);
-        fn glActiveTexture(tex: GLenum);
-        fn glDeleteTextures(n: GLsizei, textures: *mut GLuint);
-
-        // rendering
-        fn glViewport(x: GLint, y: GLint, w: GLsizei, h: GLsizei);
-        fn glEnable(cap: GLenum);
-        fn glDisable(cap: GLenum);
-        fn glClear(mask: GLbitfield);
-        fn glClearColor(red: GLfloat, green: GLfloat, blue: GLfloat, alpha: GLfloat);
-        fn glDepthMask(flag: GLboolean);
-        fn glBlendFunc(sfactor: GLenum, dfactor: GLenum);
-        fn glBlendEquation(mode: GLenum);
-        fn glUniform1f(loc: GLint, v: GLfloat);
-        fn glUniform2f(loc: GLint, v0: GLfloat, v1: GLfloat);
-        fn glUniform4fv(loc: GLint, count: GLsizei, value: *const GLfloat);
-        fn glUniformMatrix3fv(loc: GLint, count: GLsizei, transpose: GLboolean, value: *const GLfloat);
-        fn glGenBuffers(n: GLsizei, bufs: *mut GLuint);
-        fn glBindBuffer(target: GLenum, buf: GLuint);
-        fn glBufferData(target: GLenum, size: GLsizeiptr, data: *const c_void, usage: GLenum);
-        fn glEnableVertexAttribArray(index: GLuint);
-        fn glVertexAttribPointer(index: GLuint, size: GLint, kind: GLenum, normalized: GLboolean, stride: GLsizei, ptr: *const c_void);
-        fn glDrawArrays(mode: GLenum, first: GLint, count: GLsizei);
-        fn glDeleteBuffers(n: GLsizei, bufs: *mut GLuint);
     }
 }
