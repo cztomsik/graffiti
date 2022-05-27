eac5fcf4958bdb0cddcfe6c7e255f12525e8822b
Kamil Tomšík <info@tomsik.cz>
2022-05-27 23:17:25 +0200

    use skia for rendering, render tree is just vec of edges

--- libgraffiti/src/lib.rs
+++ libgraffiti/src/lib.rs
@@ -1,19 +1,4 @@
 #![warn(clippy::all, clippy::pedantic)]
-#![allow(
-    unused,
-    clippy::module_name_repetitions,
-    clippy::must_use_candidate,
-    clippy::missing_panics_doc,
-    clippy::wildcard_imports,
-    clippy::missing_safety_doc,
-    clippy::cast_possible_truncation,
-    clippy::cast_precision_loss,
-    clippy::cast_lossless,
-    clippy::cast_sign_loss,
-    clippy::enum_glob_use,
-    clippy::missing_errors_doc,
-    clippy::type_complexity
-)]
 
 #[macro_use]
 mod util;
