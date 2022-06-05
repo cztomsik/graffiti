db0441e7a0929bf714b49c2eff1c842092541f6b
Kamil Tomšík <info@tomsik.cz>
2022-06-05 09:59:38 +0200

    WIP: viewport in hello

--- libgraffiti/examples/hello.rs
+++ libgraffiti/examples/hello.rs
@@ -1,34 +1,58 @@
 use glfw::{Context, Glfw, Window, WindowEvent};
-use graffiti::renderer::{ContainerStyle, Outline, RenderEdge, Renderer, Shadow, StrokeStyle};
-use skia_safe::{
-    textlayout::{FontCollection, Paragraph, ParagraphBuilder, ParagraphStyle, TextStyle},
-    Color, FontMgr, Paint, Rect,
-};
+use graffiti::{Document, Renderer, Viewport};
 use std::sync::mpsc::Receiver;
 
 fn main() {
     let (mut glfw, mut win, events) = create_window();
-    let mut renderer = Renderer::new(win.get_size());
-
-    let para = create_para("Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. 😀");
-
-    let tick = |win: &mut Window, renderer: &mut Renderer| {
-        renderer.render(&build_render_tree(&para));
-        win.swap_buffers();
-    };
+    // TODO: fb_size might be different and maybe we should not call renderer.resize() from viewport
+    let mut viewport = Viewport::new(win.get_size(), parse(), Renderer::new(win.get_size()));
 
     while !win.should_close() {
         glfw.wait_events();
 
         for (_, event) in glfw::flush_messages(&events) {
             match event {
-                WindowEvent::FramebufferSize(width, height) => renderer.resize((width, height)),
+                WindowEvent::CursorPos(x, y) => {} // viewport.move(x, y)
+                // TODO: click, scroll, tab_next/prev, ...
+                WindowEvent::Size(width, height) => viewport.resize((width, height)),
                 _ => println!("{:?}", &event),
             }
         }
 
-        tick(&mut win, &mut renderer);
+        viewport.render();
+        win.swap_buffers();
     }
+}
+
+fn parse() -> Document {
+    let mut doc = Document::new();
+
+    let body = doc.create_element("body");
+    let h1 = doc.create_element("h1");
+    let hello = doc.create_text_node("Hello 🚀");
+    let panel = doc.create_element("div");
+    let text = doc.create_text_node("Some text which wraps but is too long anyway so it gets clipped.....................................................");
+
+    doc.append_child(Document::ROOT, body);
+    doc.append_child(body, h1);
+    doc.append_child(h1, hello);
+    doc.append_child(body, panel);
+    doc.append_child(panel, text);
+
+    doc.set_style(
+        body,
+        "height: 300px; background-color: #ccf; padding-top: 20px; padding-left: 20px",
+    );
+    doc.set_style(
+        h1,
+        "height: 200px; outline-style: solid; padding-top: 20px; padding-left: 20px;",
+    );
+    doc.set_style(
+        panel,
+        "height: 100px; padding-top: 20px; padding-left: 20px; background-color: #fff; border-top-left-radius: 10px; overflow-x: hidden; box-shadow: 0 0 10px #000",
+    );
+
+    doc
 }
 
 fn create_window() -> (Glfw, Window, Receiver<(f64, WindowEvent)>) {
@@ -51,61 +75,4 @@ fn init_glfw() -> Glfw {
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
 
     glfw
-}
-
-fn create_para(s: &str) -> Paragraph {
-    let mut font_collection = FontCollection::new();
-    font_collection.set_default_font_manager(FontMgr::new(), None);
-    let paragraph_style = ParagraphStyle::new();
-    let mut paragraph_builder = ParagraphBuilder::new(&paragraph_style, font_collection);
-    let mut ts = TextStyle::new();
-    ts.set_foreground_color(Paint::default());
-    paragraph_builder.push_style(&ts);
-    paragraph_builder.add_text(s);
-    let mut paragraph = paragraph_builder.build();
-    paragraph.layout(256.0);
-
-    paragraph
-}
-
-fn build_render_tree<'a>(para: &'a Paragraph) -> Vec<RenderEdge<&'a Paragraph>> {
-    vec![
-        RenderEdge::OpenContainer(
-            Rect::new(10., 10., 390., 290.),
-            ContainerStyle {
-                bg_color: Some(Color::new(0xFFDDDDDD)),
-                ..Default::default()
-            },
-        ),
-        RenderEdge::OpenContainer(
-            Rect::new(10., 10., 370., 270.),
-            ContainerStyle {
-                bg_color: Some(Color::new(0xFFAAAAFF)),
-                outline: Some(Outline(1., Some(StrokeStyle::Solid), Color::BLUE)),
-                ..Default::default()
-            },
-        ),
-        RenderEdge::Text(Rect::new(10., 10., 300., 200.), &para),
-        RenderEdge::OpenContainer(
-            Rect::new(10., 150., 100., 200.),
-            ContainerStyle {
-                border_radii: Some([2., 2., 2., 2.]),
-                shadow: Some(Shadow((0., 1.), 10., 0., Color::new(0x66000000))),
-                clip: true,
-                bg_color: Some(Color::new(0xFFFFFFFF)),
-                ..Default::default()
-            },
-        ),
-        RenderEdge::OpenContainer(
-            Rect::new(10., 10., 300., 300.),
-            ContainerStyle {
-                bg_color: Some(Color::new(0xFFDDDDDD)),
-                ..Default::default()
-            },
-        ),
-        RenderEdge::CloseContainer,
-        RenderEdge::CloseContainer,
-        RenderEdge::CloseContainer,
-        RenderEdge::CloseContainer,
-    ]
 }
