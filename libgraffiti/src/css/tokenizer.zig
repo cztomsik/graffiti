882997cb0975499c0393bcb073b68c115470ad31
Kamil Tomšík <info@tomsik.cz>
2022-08-10 21:33:54 +0200

    extend tokenizer

--- libgraffiti/src/css/tokenizer.zig
+++ libgraffiti/src/css/tokenizer.zig
@@ -5,19 +5,13 @@ const std = @import("std");
 pub const Token = union(enum) {
     ident: []const u8,
     function: []const u8,
-    at_keyword: []const u8,
-    hash, //: []const u8,
-    string: []const u8,
-    bad_string: []const u8,
-    url: []const u8,
-    bad_url: []const u8,
-    delim: u8,
     number: f32,
     percentage: f32,
     dimension: struct { value: f32, unit: []const u8 },
+    hash: []const u8,
+    string: []const u8,
+    // at_keyword: []const u8,
     space,
-    CDO,
-    CDC,
     colon,
     semi,
     comma,
@@ -27,6 +21,7 @@ pub const Token = union(enum) {
     rparen,
     lcurly,
     rcurly,
+    delim: u8,
 };
 
 const TokenTag = @typeInfo(Token).Union.tag_type.?;
@@ -42,29 +37,28 @@ pub const Tokenizer = struct {
     }
 
     pub fn next(self: *Self) !Token {
-        const ch = try self.peek(0);
-
-        // if (ch == '/' and (self.peek(1) catch 0) == '*') {
-        //     try self.skipComment();
-        //     return try self.next();
-        // }
+        const ch = try self.nextCharSkipComments();
 
         if (isIdentStart(ch)) {
-            return Token{ .ident = self.consumeIdent() };
+            const ident = self.consume(isIdent);
 
-            // TODO: if (next == '(') token = .{ .function = token.ident }
-            // TODO: check if function is url() and replace with url token
+            if ((self.peek(0) catch 0) == '(') {
+                self.pos += 1;
+                return Token{ .function = ident };
+            }
+
+            return Token{ .ident = ident };
         }
 
         if (isNumeric(ch)) {
-            const num = try self.consumeNumber();
+            const num = try std.fmt.parseFloat(f32, self.consume(isNumeric));
 
             if (self.peek(0) catch 0 == '%') {
                 return Token{ .percentage = num };
             }
 
             if (isIdentStart(self.peek(0) catch 0)) {
-                return Token{ .dimension = .{ .value = num, .unit = self.consumeIdent() } };
+                return Token{ .dimension = .{ .value = num, .unit = self.consume(isIdent) } };
             }
 
             return Token{ .number = num };
@@ -74,7 +68,7 @@ pub const Tokenizer = struct {
         return switch (ch) {
             '\'', '"' => .{ .string = try self.consumeString(ch) },
             ' ', '\t', '\r', '\n' => .space,
-            '#' => return error.TODO,
+            '#' => Token{ .hash = self.consume(isIdent) },
             ':' => Token.colon,
             ';' => Token.semi,
             ',' => Token.comma,
@@ -90,28 +84,27 @@ pub const Tokenizer = struct {
         // TODO: maybe skipSpaces() and no token type?
         // '.', '+' => self.consumeNumber() catch .{ .delim = ch },
         // '-' => self.consumeNumber() catch (self.consumeIdent() catch .{ .delim = ch }),
-        // '#' => .hash, // TODO: hash_num/hash_id
-        // // TODO: if hash, try hex num & ident
     }
 
-    fn skipComment(self: *Self) !void {
-        self.pos += 2;
-        while ((try self.peek(0)) != '*' and (try self.peek(1)) != '/') self.pos += 1;
-        self.pos += 1;
+    fn nextCharSkipComments(self: *Self) !u8 {
+        while (true) {
+            const ch = try self.peek(0);
+
+            if (ch == '/' and (self.peek(1) catch 0) == '*') {
+                self.pos += 2;
+                while ((try self.peek(0)) != '*' and (try self.peek(1)) != '/') self.pos += 1;
+                self.pos += 2;
+            } else {
+                return ch;
+            }
+        }
     }
 
-    fn consumeIdent(self: *Self) []const u8 {
+    fn consume(self: *Self, fun: anytype) []const u8 {
         const start = self.pos;
-        while (isIdent(self.peek(0) catch 0)) self.pos += 1;
+        while (fun(self.peek(0) catch 0)) self.pos += 1;
 
         return self.input[start..self.pos];
-    }
-
-    fn consumeNumber(self: *Self) !f32 {
-        const start = self.pos;
-        while (isNumeric(self.peek(0) catch 0)) self.pos += 1;
-
-        return std.fmt.parseFloat(f32, self.input[start..self.pos]);
     }
 
     fn consumeString(self: *Self, quote: u8) ![]const u8 {
@@ -157,6 +150,7 @@ fn expectTokens(input: []const u8, tokens: []const TokenTag) !void {
         try std.testing.expectEqual(tag, try tokenizer.next());
     }
 
+    try std.testing.expectError(error.eof, tokenizer.next());
     try std.testing.expectEqual(input.len, tokenizer.pos);
 }
 
@@ -165,8 +159,8 @@ test {
     try expectTokens(" ", &.{.space});
     // try expectTokens(" \n \t \n ", &.{.space});
 
-    // try expectTokens("/* */", &.{.comment});
-    // try expectTokens(" /**/ /**/ ", &.{ .space, .comment, .space, .comment, .space });
+    try expectTokens("/* */", &.{});
+    try expectTokens(" /**/ /**/ ", &.{ .space, .space, .space });
 
     try expectTokens(";", &.{.semi});
     try expectTokens(";;", &.{ .semi, .semi });
@@ -181,7 +175,7 @@ test {
 
     try expectTokens("ff0", &.{.ident});
     try expectTokens("00f", &.{.dimension});
-    // try expectTokens("#00f", &.{.hash});
+    try expectTokens("#00f", &.{.hash});
 
     try expectTokens("0 10px", &.{ .number, .space, .dimension });
     try expectTokens("0 0 10px 0", &.{ .number, .space, .number, .space, .dimension, .space, .number });
@@ -204,7 +198,7 @@ test {
     try expectTokens("'foo'", &.{.string});
     try expectTokens("\"foo bar\"", &.{.string});
     try expectTokens("'\\''", &.{.string});
-    // try expectTokens("prop: url('foo bar')", &.{ .ident, .colon, .space, .function, .string, .rparen });
+    try expectTokens("prop: url('foo bar')", &.{ .ident, .colon, .space, .function, .string, .rparen });
 
     // assert_eq!(tokenize(b"[foo=\"bar\"]"), vec!["[", "foo", "=", "\"bar\"", "]"]);
 
