b6ddf91c07581683bd757256f1c8c8f32d76fe64
Kamil Tomšík <info@tomsik.cz>
2022-10-02 09:22:56 +0200

    cleanup

--- src/lib.zig
+++
@@ -1,4 +0,0 @@
-pub const App = @import("app.zig").App;
-pub const Window = @import("window.zig").Window;
-pub const Canvas = @import("gfx/canvas.zig").Canvas;
-pub const DomView = @import("dom_view.zig").DomView;
