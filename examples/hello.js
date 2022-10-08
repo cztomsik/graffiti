84a275b2b1d52557a3cead8fe1ab4fa293e5b7fd
Kamil Tomšík <info@tomsik.cz>
2022-10-08 19:02:37 +0200

    update example

--- examples/hello.js
+++ examples/hello.js
@@ -1,3 +1,7 @@
 import '../graffiti.js'
 
+document.body.style.cssText = 'background-color: #f00; opacity: 0.5'
+
 document.body.appendChild(document.createTextNode('Hello'))
+
+setInterval(() => document.body.appendChild(document.createTextNode('...')), 1000)
