1185449509f007d5aef17577823fdaf3bb135bf8
Kamil Tomšík <info@tomsik.cz>
2022-10-19 21:50:36 +0200

    add todo

--- examples/hello.js
+++ examples/hello.js
@@ -4,4 +4,6 @@ document.body.style.cssText = 'background-color: #f00; opacity: 0.5'
 
 document.body.appendChild(document.createTextNode('Hello'))
 
-setInterval(() => document.body.appendChild(document.createTextNode('...')), 1000)
+// TODO: if we stop tick()ing after close then this would still keep
+//       nodejs alive, which means app is running but we are not consuming events anymore -> crash
+//setInterval(() => document.body.appendChild(document.createTextNode('...')), 1000)
