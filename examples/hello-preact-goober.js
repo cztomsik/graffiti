45004058e399d197063e312c8b2b65753b9d0b8a
Kamil Tomsik <info@tomsik.cz>
2023-06-07 17:29:41 +0200

    fix comment

--- examples/hello-preact-goober.js
+++ examples/hello-preact-goober.js
@@ -1,4 +1,4 @@
-// node --experimental-network-imports examples/hello-preact
+// node --experimental-network-imports examples/hello-preact-goober
 import 'graffiti'
 import { html, render } from 'https://unpkg.com/htm/preact/standalone.mjs'
 import { css } from 'https://unpkg.com/goober/dist/goober.esm.js'
